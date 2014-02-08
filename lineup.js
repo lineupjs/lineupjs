4d5181076a6adbf30ddee7fe988b554163f2fae1
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-02-08 15:34:53 +0100

    simple table based version with sorting by column and bars

--- lineup.js
+++ lineup.js
@@ -1,122 +1,135 @@
-/**
- * Alexander Lex - alex@seas.harvard.edu Inspired by
- * http://bl.ocks.org/mbostock/3885705
- */
-
-var data = [ 15, 23, 16, 14, 32, 30 ];
-var data2 = [ 18, 25, 17, 12, 85, 22 ];
-var schoolName = "schoolName";
-var academic = "academic";
-var data;
-
-function dataload(dataTmp) {
-
-	data = dataTmp;
-
-	// data.forEach(function(d) {
-	// d.academic = +d.academic;
-	// });
-	plot();
+function LineUpColumn(desc) {
+	this._desc = desc;
+	this.column = desc.column;
+	this.color = desc.color;
+	this.width = desc.width || 100;
+	this.label= desc.label || desc.column;
 }
+LineUpColumn.prototype = {
+	load : function($cell, value) {
+		this.update($cell, value)
+	},
+	update : function($cell, value) {
+		$cell.text(value)	
+	},
+	loadTh : function($cell) {
+		$cell.attr("id",this.column);
+		$cell.text(this.label)
+	},
+	loadCol : function($col) {
+		$col.attr("width",this.width)
+	},
+	sortBy : function(a,b) {
+		var va = a[this.column];
+		var vb = b[this.column];
+		return d3.ascending(va,vb);
+	}
+};
 
-function plot() {
+function LineUpNumber(desc) {
+	LineUpColumn.call(this,desc)
 
-	var barHeight = 15;
-	var barSpacing = 3;
-	var textWidth = 200;
+	this.scale = d3.scale.linear().domain(desc.domain).range([0,1]);
+}
+LineUpNumber.prototype = $.extend({},LineUpColumn.prototype,{
+	load : function($cell, value) {
+		$cell.append("div");
+		this.update($cell, value);
+	},
+	update : function($cell, value) {
+		var n = +value;
+		$cell.select("div")
+			.style("width",this.scale(n)*100+"%")
+			.style("background-color",this.color)
+			.text(value);
+	},
+	sortBy : function(a,b) {
+		var va = +a[this.column];
+		var vb = +b[this.column];
+		return d3.descending(va,vb);
+	}
+});
 
-	var paddingTop = 30;
-	var paddingSide = 20;
+function LineUpString(desc) {
+	LineUpColumn.call(this,desc)
+}
+LineUpString.prototype = $.extend({},LineUpColumn.prototype, {
 
-	var truncateAfter = 25;
+});
 
-	var w = 700;
-	var h = data.length * (barHeight + barSpacing) + 2 * paddingTop;
 
-	// var yScale = d3.scale.ordinal().domain(d3.range(data.length))
-	// .rangeRoundBands([ 0, h ], 0.3);
+function LineUp(tableId, file, columns) {
+	var $table = d3.select(tableId).classed("lineup",true);
+	var $thead = $table.append("thead");
+	var $tbody = $table.append("tbody");
+	var data = [];
 
-	var yScale = d3.scale.ordinal().rangeRoundBands([ paddingTop, h ], .1);
-	var xScale = d3.scale.linear().domain([ 0, d3.max(data, function(d) {
-		return d.academic;
-	}) ]).range([ 0, w - textWidth - paddingSide ]);
+	var colTypes = {
+		"number" : LineUpNumber,
+		"string" : LineUpString,
+	};
 
-	yScale.domain(data.map(function(d) {
-		return d.schoolname;
-	}));
-
-	var xAxis = d3.svg.axis().scale(xScale).orient("top");
-
-	var svg = d3.select("body").append("svg").attr("width", w)
-			.attr("height", h);
-
-	// background color
-	svg.append("rect").attr({
-		id : "base",
-		width : w,
-		height : h,
-		fill : "#eeeeee"
+	var colors = d3.scale.category10();
+	this.cols = columns.map(function(c,i) {
+		c.color = colors(i);
+		return new colTypes[c.type](c)
 	});
 
-	svg.append("g").attr("class", "axis").attr("transform",
-			"translate(" + textWidth + ", " + (paddingTop - 5) + ")").attr(
-			"class", "x axis").call(xAxis);
-
-	var rectangles = svg.selectAll(".bar").data(data).enter().append("rect")
-			.attr({
-				class : "bar",
-				width : function(d) {
-					console.log(d.academic);
-					return xScale(d.academic);
-				},
-				height : yScale.rangeBand,
-				y : function(d) {
-					return yScale(d.schoolname);
-				},
-				x : textWidth
+	//create header line
+	$table.selectAll("col").data(cols)
+		.enter()
+			.append("col")
+				.each(function(col) { 
+					col.loadCol(d3.select(this)); 
+				})
+	
+	//create header line
+	$thead.append("tr").selectAll("th").data(cols)
+		.enter()
+			.append("th")
+				.each(function(x) { x.loadTh(d3.select(this)); })
+				.on("click", function(col) {
+					d3.selectAll("th").classed("sortBy",false)
+					d3.select("th#"+col.column).classed("sortBy",true)
+					sortBy(function(a,b) { return col.sortBy(a,b) }); 
+				});
+		
+	function sortBy(cmp) {
+		data.sort(cmp)
+		update()
+	}
+	function columnRow(row) {
+		return cols.map(function(c) { return { desc : c, value : row[c.column]}})
+	}
+	
+	function updateLine(row) {
+		var $line = d3.select(this).selectAll("td").data(columnRow(row))
+			.each(function(c) {
+				c.desc.update(d3.select(this),c.value)
 			});
+		$line.exit()
+			.remove();
+		$line.enter()
+			.append("td")
+			.each(function(c) {
+				c.desc.load(d3.select(this),c.value);
+				return this;
+			});
+	}
 
-	svg.selectAll(".barLabel").data(data).enter().append("text").text(
-			function(d) {
-				return d.schoolname.substring(0, truncateAfter);
-			}).attr({
-		class : "barLabel",
-		y : function(d) {
-			return yScale(d.schoolname) + 15;
-		},
-		x : paddingSide,
-		height : yScale.rangeBand,
-		'text-anchor' : 'left'
+	function update() {
+		//create content lines
+		var $row = $tbody.selectAll("tr").data(data.slice(0,100));
+		$row.each(updateLine);
+		$row.exit()
+			.remove();
+		$row.enter()
+			.append("tr").each(updateLine);
+		$row.order();
+	}
+
+	d3.tsv(file, function(_data) {
+		data = _data;
+		update();
 	});
-
-	d3.select("#update-list").on(
-			"click",
-			function() {
-
-				data.sort(function(a, b) {
-					return b.academic - a.academic;
-				});
-
-				console.log(data, function(d) {
-					d.academic
-				});
-
-				yScale.domain(data.map(function(d) {
-					return d.schoolname;
-				}));
-
-				svg.selectAll(".barLabel").transition().duration(1000).attr(
-						"y", function(d) {
-							return yScale(d.schoolname) + 15;
-						});
-
-				svg.selectAll(".bar").transition().duration(1000).attr("y",
-						function(d) {
-							return yScale(d.schoolname);
-						});
-
-			});
-
 }
-
-d3.tsv("wur2013.txt", dataload);
