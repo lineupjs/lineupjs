0c5148c4a6f2ec134bfd6bf942921a70934121c1
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-02-08 18:48:22 +0100

    stacked column simple version + rank column

--- lineup.js
+++ lineup.js
@@ -6,11 +6,11 @@ function LineUpColumn(desc) {
 	this.id = desc.id || this.column;
 }
 LineUpColumn.prototype = {
-	load : function($cell, row) {
-		this.update($cell, row)
+	load : function($cell, row, i) {
+		this.update($cell, row, i)
 	},
-	update : function($cell, row) {
-		$cell.text(this.get(row))	
+	update : function($cell, row, i) {
+		$cell.text(this.get(row, i))	
 	},
 	loadTh : function($cell) {
 		$cell.attr("id",this.id)
@@ -78,9 +78,25 @@ LineUpString.prototype = $.extend({},LineUpColumn.prototype, {
 	}
 });
 
+function LineUpRank(desc) {
+	LineUpColumn.call(this,desc)
+	this.width = desc.width || 40;
+	this.id = "rank";
+	this.label = desc.label || "Rank";
+}
+LineUpRank.prototype = $.extend({},LineUpColumn.prototype, {
+	get : function(row, i) {
+		return i+1;
+	},
+	sortBy : undefined
+});
+
 function LineUpComposite(desc, toColumn) {
 	LineUpColumn.call(this,desc)
 	this.children = desc.children.map(toColumn);
+	for (var i = this.children.length - 1; i >= 0; i--) {
+		this.children[i].parent = this;
+	};
 }
 LineUpComposite.prototype = $.extend({},LineUpColumn.prototype, {
 	
@@ -123,7 +139,16 @@ LineUpMax.prototype = $.extend({},LineUpComposite.prototype, LineUpNumber.protot
 
 function LineUpStacked(desc,toColumn) {
 	LineUpComposite.call(this,desc,toColumn)
+	this.hierarchical = this.children;
+	this.compressed = false;
 	this.filter = desc.filter || [0,1];
+	this.width = 0;
+	for (var i = this.children.length - 1; i >= 0; i--) {
+		var child = this.children[i];
+		this.width += child.width;
+		if (child.hierarchical)
+			child.compressed = true;
+	};
 }
 LineUpStacked.prototype = $.extend({},LineUpComposite.prototype, LineUpNumber.prototype, {
 	weights : function() {
@@ -165,49 +190,95 @@ function LineUp(tableId, file, columns) {
 		"string" : LineUpString,
 		"max" : LineUpMax,
 		"stacked" : LineUpStacked,
+		"rank" : LineUpRank,
 	};
 
 	function toColumn(desc) {
 		return new colTypes[desc.type](desc, toColumn);
 	}
-	this.cols = columns.map(toColumn);
+	var cols = columns.map(toColumn);
 
-	//create header line
-	$table.selectAll("col").data(cols)
-		.enter()
-			.append("col")
-				.each(function(col) { 
-					col.loadCol(d3.select(this)); 
-				})
+	function flatCols() {
+		var r = [];
+		function flatImpl(c) {
+			c.forEach(function(cx) {
+				if (cx.hierarchical && !cx.compressed) {
+					flatImpl(cx.hierarchical);
+				} else {
+					r.push(cx);
+				}
+			})
+		}
+		flatImpl(cols)
+		return r;
+	}
 	
-	//create header line
-	$thead.append("tr").selectAll("th").data(cols)
-		.enter()
-			.append("th")
-				.each(function(x) { x.loadTh(d3.select(this)); })
-				.on("click", function(col) {
-					d3.selectAll("th").classed("sortBy",false)
-					d3.select("th#"+col.column).classed("sortBy",true)
-					sortBy(function(a,b) { return col.sortBy(a,b) }); 
-				});
+
+	(function() {
+		//compute nesting level
+		var hasSecondLevel = false;
+		cols.forEach(function(c) { if (c.hierarchical && !c.compressed) hasSecondLevel = true });
+		//create header line
+		var flatcols = flatCols();
+		$table.selectAll("col").data(flatcols)
+			.enter()
+				.append("col")
+					.each(function(col) { 
+						col.loadCol(d3.select(this)); 
+					})
+		
+		function fillTh(col) {
+			var $th = d3.select(this);
+			col.loadTh($th);
+			if (col.sortBy)
+				$th.on("click", function(col) {
+						d3.selectAll("th").classed("sortBy",false)
+						d3.select("th#"+col.column).classed("sortBy",true)
+						sortBy(function(a,b) { return col.sortBy(a,b) }); 
+					})
+		}
+		//create header lin e
+		$thead.append("tr").selectAll("th").data(cols)
+			.enter()
+				.append("th")
+				.each(fillTh)
+				.each(function(x) { 
+					var $th = d3.select(this);
+					if (hasSecondLevel && !(x.hierarchical && !x.compressed))
+						$th.attr("rowspan",2)
+					if ((x.hierarchical && !x.compressed))
+						$th.attr("colspan",x.hierarchical.length)
+				})
+		if (hasSecondLevel) {
+			$thead.append("tr").selectAll("th").data(flatcols)
+			.enter()
+				.append("th")
+				.each(fillTh)
+				.each(function(x) { 
+					if (!x.parent)
+						d3.select(this).remove();
+				})
+		}
+	})()
+	
 		
 	function sortBy(cmp) {
 		data.sort(cmp)
 		update()
 	}
-	
-	function updateLine(row) {
-		var $line = d3.select(this).selectAll("td").data(cols)
+
+	function updateLine(row, i, flatcols) {
+		var $row = d3.select(this);
+		var $line = $row.selectAll("td").data(flatcols)
 			.each(function(c) {
-				c.update(d3.select(this),row)
+				c.update(d3.select(this),row, i)
 			});
 		$line.exit()
 			.remove();
 		$line.enter()
 			.append("td")
 			.each(function(c) {
-				c.load(d3.select(this),row);
-				return this;
+				c.load(d3.select(this),row, i);
 			});
 	}
 
@@ -223,11 +294,17 @@ function LineUp(tableId, file, columns) {
 		});
 		d = d.slice(0,100);
 		var $row = $tbody.selectAll("tr").data(d);
-		$row.each(updateLine);
+
+		var flatcols = flatCols();
+		function tmp(x, i) {
+			return updateLine.call(this,x,i,flatcols);
+		}
+		
+		$row.each(tmp);
 		$row.exit()
 			.remove();
 		$row.enter()
-			.append("tr").each(updateLine);
+			.append("tr").each(tmp);
 		$row.order();
 	}
 
