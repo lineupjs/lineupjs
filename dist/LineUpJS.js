342ce79f0a898c444f0651dafb261bfe674bab4f
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-10-17 14:21:26 +0200

    update dist

--- dist/LineUpJS.js
+++ dist/LineUpJS.js
@@ -120,6 +120,10 @@ var LineUp;
     },
     /* enables manipulation features, remove column, reorder,... */
     manipulative: true,
+    interaction: {
+      //enable the table tooltips
+      tooltips: true
+    },
     filter: {
       skip: 0,
       limit: Number.POSITIVE_INFINITY,
@@ -146,6 +150,29 @@ var LineUp;
     //initial sort
     this.storage.resortData({});
     this.updateAll();
+  };
+
+  LineUp.prototype.assignColors = function (headers) {
+    //Color schemes are in config (.columnColors / .grayColor)
+
+    // clear map
+    var config = this.config;
+    config.colorMapping = d3.map();
+
+    var colCounter = 0;
+
+    headers.forEach(function (d) {
+      if (d.color) {
+        config.colorMapping.set(d.id, d.color);
+      } else if ((d instanceof LineUp.LineUpStringColumn) || (d.id === "rank")) {
+        // gray columns are:
+        config.colorMapping.set(d.id, config.grayColor);
+      } else {
+        config.colorMapping.set(d.id, config.columnColors(colCounter));
+        colCounter++;
+      }
+    });
+    //console.log(config.colorMapping);
   };
 
   LineUp.prototype.updateAll = function (stackTransition) {
@@ -251,7 +278,8 @@ var LineUp;
   function LineUpColumn(desc) {
     this.column = desc.column;
     this.label = desc.label || desc.column;
-    this.id = desc.id || this.column;
+    this.color = desc.color;
+    this.id = (desc.id || this.column).replace(/[\s!\'#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '_'); //replace non css stuff to _
     this.missingValue = desc.missingValue;
     this.layout = {};
   }
@@ -1513,78 +1541,6 @@ var LineUp;
 /* global d3 */
 var LineUp;
 (function (LineUp, d3) {
-  LineUp.prototype = LineUp.prototype || {};
-  LineUp.prototype.layoutHeaders = function (headers) {
-    var offset = 0;
-    var config = this.config;
-
-    headers.forEach(function (d) {
-//        console.log(d);
-      d.offsetX = offset;
-      d.offsetY = 2;
-      d.height = config.htmlLayout.headerHeight - 4;
-      offset += d.getColumnWidth();
-
-//        console.log(d.getColumnWidth());
-    });
-
-    //console.log("layout Headers:", headers);
-
-    //update all the plusSigns shifts
-    var shift = offset + 4;
-    d3.values(config.svgLayout.plusSigns).forEach(function (addSign) {
-      addSign.x = shift;
-      shift += addSign.w + 4;
-    });
-
-    headers.filter(function (d) {
-      return (d instanceof LineUp.LayoutStackedColumn);
-    })
-      .forEach(function (d) {
-
-        d.height = config.htmlLayout.headerHeight / 2 - 4;
-
-        var localOffset = 0;
-        var parentOffset = d.offsetX;
-        var allChilds = d.children.concat(d.emptyColumns);
-        allChilds.map(function (child) {
-          child.offsetX = parentOffset + localOffset;
-          child.localOffsetX = localOffset;
-          localOffset += child.getColumnWidth();
-
-          child.offsetY = config.htmlLayout.headerHeight / 2 + 2;
-          child.height = config.htmlLayout.headerHeight / 2 - 4;
-        });
-      });
-    this.totalWidth = shift;
-  };
-
-  LineUp.prototype.assignColors = function (headers) {
-    //Color schemes are in config (.columnColors / .grayColor)
-
-
-    // clear map
-    var config = this.config;
-    config.colorMapping = d3.map();
-
-    var colCounter = 0;
-
-    headers.forEach(function (d) {
-      // gray columns are:
-      if ((d instanceof LineUp.LineUpStringColumn) || (d.id === "rank")) {
-        config.colorMapping.set(d.id, config.grayColor);
-      } else {
-//            console.log(config.columnColors(colCounter), colCounter, d);
-        config.colorMapping.set(d.id, config.columnColors(colCounter));
-        colCounter++;
-      }
-    });
-    //console.log(config.colorMapping);
-  };
-}(LineUp || (LineUp = {}), d3));
-/* global d3 */
-var LineUp;
-(function (LineUp, d3) {
 
   function addLine($svg, x1, y1, x2, y2, clazz) {
     return $svg.append("line").attr({
@@ -2652,20 +2608,25 @@ var LineUp;
           point = point.matrixTransform(matrix);
           return scrollTop + point.y;
         }
-
-        that.tooltip.show(generateTooltip(row, allHeaders, that.config), {
-          x: d3.event.x + 10,
-          y: absoluteRowPos(this),
-          height: that.config.svgLayout.rowHeight
-        });
+        if (that.config.interaction.tooltips) {
+          that.tooltip.show(generateTooltip(row, allHeaders, that.config), {
+            x: d3.event.x + 10,
+            y: absoluteRowPos(this),
+            height: that.config.svgLayout.rowHeight
+          });
+        }
       },
       mousemove: function () {
-        that.tooltip.move({
-          x: d3.event.x
-        });
+        if (that.config.interaction.tooltips) {
+          that.tooltip.move({
+            x: d3.event.x
+          });
+        }
       },
       mouseleave: function () {
-        that.tooltip.hide();
+        if (that.config.interaction.tooltips) {
+          that.tooltip.hide();
+        }
         d3.select(this).classed('hover', false);
         d3.select(this).selectAll('text.hoveronly').remove();
       }
@@ -2694,6 +2655,52 @@ var LineUp;
 var LineUp;
 (function (LineUp, d3, $, undefined) {
   LineUp.prototype = LineUp.prototype || {};
+
+  LineUp.prototype.layoutHeaders = function (headers) {
+    var offset = 0;
+    var config = this.config;
+
+    headers.forEach(function (d) {
+//        console.log(d);
+      d.offsetX = offset;
+      d.offsetY = 2;
+      d.height = config.htmlLayout.headerHeight - 4;
+      offset += d.getColumnWidth();
+
+//        console.log(d.getColumnWidth());
+    });
+
+    //console.log("layout Headers:", headers);
+
+    //update all the plusSigns shifts
+    var shift = offset + 4;
+    d3.values(config.svgLayout.plusSigns).forEach(function (addSign) {
+      addSign.x = shift;
+      shift += addSign.w + 4;
+    });
+
+    headers.filter(function (d) {
+      return (d instanceof LineUp.LayoutStackedColumn);
+    })
+      .forEach(function (d) {
+
+        d.height = config.htmlLayout.headerHeight / 2 - 4;
+
+        var localOffset = 0;
+        var parentOffset = d.offsetX;
+        var allChilds = d.children.concat(d.emptyColumns);
+        allChilds.map(function (child) {
+          child.offsetX = parentOffset + localOffset;
+          child.localOffsetX = localOffset;
+          localOffset += child.getColumnWidth();
+
+          child.offsetY = config.htmlLayout.headerHeight / 2 + 2;
+          child.height = config.htmlLayout.headerHeight / 2 - 4;
+        });
+      });
+    this.totalWidth = shift;
+  };
+
   /**
    * Render the given headers
    * @param headers - the array of headers, see {@link LineUpColumn}
