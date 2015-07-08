e9fa1958bdbc97be844b5ca61fee227756b62086
Hendrik Strobelt <hendrik@strobelt.com>
2015-07-08 16:41:39 -0400

    fix for config.storage during create process

--- dist/LineUpJS.js
+++ dist/LineUpJS.js
@@ -90,9 +90,9 @@ var LineUp;
 
   LineUp.prototype = LineUpClass.prototype = $.extend(LineUpClass.prototype, LineUp.prototype);
   LineUp.create = function (storage, $container, options) {
-    if (!$.isPlainObject(storage)) {
+    //if (!$.isPlainObject(storage)) {
       storage = { storage: storage };
-    }
+    //}
     var r = new LineUpClass(storage, $container, options);
     r.startVis();
     return r;
