be83edfcd878b3b937ec4c65e0556cc7dc8e42c0
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-08-25 10:12:20 +0200

    work on grunt script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,68 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= pkg.license %> */\n',
+    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    ' Licensed <%= pkg.license %> */\n',
     // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+
+    watch: {
+      ts: {
+        files: ['src/**.ts', 'demo/**.ts'],
+        tasks: ['compile']
       },
+      sass: {
+        files: ['src/**.scss', 'demo/**.scss'],
+        tasks: ['sass:dev']
+      }
+    },
+
+    sass: {
+      dist: {                            // target
+        files: [{
+          expand: true,
+          src: ['src/**.scss'],
+          dest: 'dist',
+          flatten: true,
+          ext: '.css'
+        }, {
+          expand: true,
+          src: ['demo/**/*.scss'],
+          dest: '',
+          ext: '.css'
+        }]
+      },
+      dev: {                              // another target
+        options: {                      // dictionary of render options
+          sourceMap: true
+        },
+        files: [{
+          expand: true,
+          src: ['src/**.scss'],
+          dest: 'dist',
+          flatten: true,
+          ext: '.css'
+        }, {
+          expand: true,
+          src: ['demo/**/*.scss'],
+          dest: '',
+          ext: '.css'
+        }]
+      }
+    },
+
+    browserify: {
       dist: {
-        src: [
-          'deploy/module_wrapper_begin.js.txt',
-          'src/lineup.js',
-          'src/lineup_datastructure.js',
-          'src/lineup_gui_helper.js',
-          'src/lineup_mappingeditor.js',
-          'src/lineup_storage.js',
-          'src/lineup_tablebody.js',
-          'src/lineup_tableheader.js',
-          'deploy/module_wrapper_end.js.txt'],
-        dest: 'dist/<%= pkg.name %>.js'
+        src: ['src/*.js'],
+        dest: 'dist/<%= pkg.name %>.js',
+        banner: '<%=banner%>'
+        // Note: The entire `browserify-shim` config is inside `package.json`.
       }
     },
     uglify: {
@@ -35,70 +70,83 @@ module.exports = function(grunt) {
         banner: '<%= banner %>'
       },
       dist: {
-        src: '<%= concat.dist.dest %>',
+        src: '<%= browserify.dist.dest %>',
         dest: 'dist/<%= pkg.name %>.min.js'
       }
     },
-    jshint: {
+    clean: ['doc', 'dist'],
+    tslint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {
-          jQuery: true
+        configuration: grunt.file.readJSON('tslint.json')
+      },
+      all: {
+        src: ['src/**.ts', 'demo/**.ts']
+      }
+    },
+    ts: {
+      // A specific target
+      dev: {
+        // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
+        src: ['src/**.ts', 'demo/**.ts'],
+        // If specified, generate this file that to can use for reference management
+        reference: 'tsd.gen.d.ts',
+        // If specified, the generate JavaScript files are placed here. Only works if out is not specified
+        //outDir: 'test/outputdirectory',
+        // If specified, watches this directory for changes, and re-runs the current target
+        //watch: '<%= yeoman.app %>/scripts',
+        // Use to override the default options, http://gruntjs.com/configuring-tasks#options
+        options: {
+          target: 'es5',
+          module: 'commonjs', // 'amd' (default) | 'commonjs'
+          sourceMap: true,
+          declaration: false,
+          removeComments: false
         }
       },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['src/**/*.js']
+      // A specific target
+      dist: {
+        src: ['src/**.ts', 'demo/**.ts'],
+        reference: 'tsd.gen.d.ts',
+        options: {
+          target: 'es5',
+          module: 'commonjs', // 'amd' (default) | 'commonjs'
+          sourceMap: false,
+          declaration: false,
+          removeComments: true
+        }
       }
     },
-    nodeunit: {
-      files: ['test/**/*_test.js']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
-      }
-    },
-      jsdoc : {
-          dist : {
-              src: ['src/*.js',"README.md"],
-              options : {
-                  destination : 'doc',
-                  template : "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template",
-                  configure : "jsdoc.conf.json"
-              }
+
+    tsd: {
+      reinstall: {
+        options: {
+          // execute a command
+          command: 'reinstall',
+
+          //optional: always get from HEAD
+          latest: false,
+
+          // specify config file
+          config: 'tsd.json',
+
+          // experimental: options to pass to tsd.API
+          opts: {
+            // props from tsd.Options
           }
-      },
-      clean: ["doc", "dist"]
+        }
+      }
+    }
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
-  grunt.registerTask('doc', ['jsdoc']);
+  grunt.registerTask('watch', ['ts:dev', 'sass:dev', 'watch']);
+  grunt.registerTask('compile', ['ts:dev', 'browserify']);
+  grunt.registerTask('default', ['clean', 'tsd:reinstall', 'ts:dist', 'sass:dist', 'tslint', 'browserify', 'concat', 'uglify']);
+
 };
