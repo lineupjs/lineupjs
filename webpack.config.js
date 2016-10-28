7b14a775996bd088311849b073b10f25ac24f444
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-28 19:58:19 +0200

    switch to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,9 @@ function generate(bundle, min) {
     },
     resolve: {
       // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+      extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
       alias: {
-        d3: '../node_modules/d3/d3'
+        d3: 'd3/d3'
       }
     },
     plugins: [
