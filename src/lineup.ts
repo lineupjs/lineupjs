e58db0ae5f19d9fd233672d67774556b9ce629c6
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-28 23:00:26 +0200

    split in index and lineup

--- src/lineup.ts
+++ src/lineup.ts
@@ -3,51 +3,17 @@
  * Created by Samuel Gratzl on 14.08.2015.
  */
 
-import './style.scss';
-import * as model_ from './model';
-import * as provider_  from './provider';
-import * as renderer_  from './renderer';
-import * as ui_  from './ui';
-import * as utils_  from './utils';
-import * as ui_dialogs_ from './ui_dialogs';
-import * as d3 from 'd3';
-
-/**
- * access to the model module
- * @type {--global-type--}
- */
-export var model = model_;
-/**
- * access to the provider module
- * @type {--global-type--}
- */
-export var provider = provider_;
-/**
- * access to the renderer module
- * @type {--global-type--}
- */
-export var renderer = renderer_;
-/**
- * access to the ui module
- * @type {--global-type--}
- */
-export var ui = ui_;
-/**
- * access to the utils module
- * @type {--global-type--}
- */
-export var utils = utils_;
-/**
- * access to the ui_dialogs module
- * @type {--global-type--}
- */
-export var ui_dialogs = ui_dialogs_;
-
+import {IColumnDesc, Column} from './model';
+import {DataProvider}  from './provider';
+import {renderers}  from './renderer';
+import {IRankingHook, dummyRankingButtonHook, PoolRenderer, IBodyRenderer, HeaderRenderer, BodyCanvasRenderer, BodyRenderer} from './ui';
+import {AEventDispatcher, ContentScroller, merge}  from './utils';
+import {scale as d3scale, selection, select, Selection} from 'd3';
 
 /**
  * main LineUp class managing data and rendering
  */
-export class LineUp extends utils_.AEventDispatcher {
+export default class LineUp extends AEventDispatcher {
   /**
    * triggered when the mouse is over a specific row
    * @argument data_index:number the selected data index or <0 if no row
@@ -112,7 +78,7 @@ export class LineUp extends utils_.AEventDispatcher {
       /**
        * hook for adding buttons to rankings in the header
        */
-      rankingButtons: <ui_.IRankingHook>ui_.dummyRankingButtonHook,
+      rankingButtons: <IRankingHook>dummyRankingButtonHook,
 
       /**
        * templates for link patterns
@@ -185,29 +151,29 @@ export class LineUp extends utils_.AEventDispatcher {
     /**
      * the renderers to use for rendering the columns
      */
-    renderers: renderer_.renderers()
+    renderers: renderers()
   };
 
-  private $container:d3.Selection<any>;
+  private $container:Selection<any>;
 
-  private body:ui_.IBodyRenderer = null;
-  private header:ui_.HeaderRenderer = null;
-  private pools:ui_.PoolRenderer[] = [];
-  private contentScroller:utils_.ContentScroller = null;
+  private body:IBodyRenderer = null;
+  private header:HeaderRenderer = null;
+  private pools:PoolRenderer[] = [];
+  private contentScroller:ContentScroller = null;
 
-  constructor(container:d3.Selection<any> | Element, public data:provider_.DataProvider, config:any = {}) {
+  constructor(container:Selection<any> | Element, public data:DataProvider, config:any = {}) {
     super();
-    this.$container = container instanceof d3.selection ? <d3.Selection<any>>container : d3.select(<Element>container);
+    this.$container = container instanceof selection ? <Selection<any>>container : select(<Element>container);
     this.$container = this.$container.append('div').classed('lu', true);
     this.config.svgLayout = this.config.body;
     this.config.htmlLayout = this.config.header;
 
-    utils.merge(this.config, config);
+    merge(this.config, config);
 
 
     this.data.on('selectionChanged.main', this.triggerSelection.bind(this));
 
-    this.header = new ui_.HeaderRenderer(data, this.node, {
+    this.header = new HeaderRenderer(data, this.node, {
       manipulative: this.config.manipulative,
       headerHeight: this.config.header.headerHeight,
       headerHistogramHeight: this.config.header.headerHistogramHeight,
@@ -222,7 +188,7 @@ export class LineUp extends utils_.AEventDispatcher {
       rankingButtons: this.config.header.rankingButtons,
       linkTemplates: this.config.header.linkTemplates
     });
-    this.body = new (this.config.body.renderer === 'svg' ? ui_.BodyRenderer: ui_.BodyCanvasRenderer)(data, this.node, this.slice.bind(this), {
+    this.body = new (this.config.body.renderer === 'svg' ? BodyRenderer: BodyCanvasRenderer)(data, this.node, this.slice.bind(this), {
       rowHeight: this.config.body.rowHeight,
       rowPadding: this.config.body.rowPadding,
       rowBarPadding: this.config.body.rowBarPadding,
@@ -241,11 +207,11 @@ export class LineUp extends utils_.AEventDispatcher {
 
     this.forward(this.body, LineUp.EVENT_HOVER_CHANGED);
     if (this.config.pool && this.config.manipulative) {
-      this.addPool(new ui_.PoolRenderer(data, this.node, this.config));
+      this.addPool(new PoolRenderer(data, this.node, this.config));
     }
 
     if (this.config.body.visibleRowsOnly) {
-      this.contentScroller = new utils_.ContentScroller(<Element>this.$container.node(), this.body.node, {
+      this.contentScroller = new ContentScroller(<Element>this.$container.node(), this.body.node, {
         backupRows: this.config.body.backupScrollRows,
         rowHeight: this.config.body.rowHeight,
         topShift: () => this.header.currentHeight()
@@ -272,13 +238,13 @@ export class LineUp extends utils_.AEventDispatcher {
    * @param node the node element to attach
    * @param config
    */
-  addPool(node:Element, config?:any):ui_.PoolRenderer;
-  addPool(pool:ui_.PoolRenderer):ui_.PoolRenderer;
-  addPool(pool_node:Element|ui_.PoolRenderer, config = this.config) {
-    if (pool_node instanceof ui_.PoolRenderer) {
-      this.pools.push(<ui_.PoolRenderer>pool_node);
+  addPool(node:Element, config?:any):PoolRenderer;
+  addPool(pool:PoolRenderer):PoolRenderer;
+  addPool(pool_node:Element|PoolRenderer, config = this.config) {
+    if (pool_node instanceof PoolRenderer) {
+      this.pools.push(<PoolRenderer>pool_node);
     } else {
-      this.pools.push(new ui_.PoolRenderer(this.data, <Element>pool_node, config));
+      this.pools.push(new PoolRenderer(this.data, <Element>pool_node, config));
     }
     return this.pools[this.pools.length - 1];
   }
@@ -315,7 +281,7 @@ export class LineUp extends utils_.AEventDispatcher {
    * @param ascending
    * @returns {boolean}
    */
-  sortBy(column:(col:model_.Column) => boolean | string, ascending = false) {
+  sortBy(column:(col:Column) => boolean | string, ascending = false) {
     var col = this.data.find(column);
     if (col) {
       col.sortByMe(ascending);
@@ -327,7 +293,7 @@ export class LineUp extends utils_.AEventDispatcher {
     return this.data.dump();
   }
 
-  changeDataStorage(data:provider_.DataProvider, dump?:any) {
+  changeDataStorage(data:DataProvider, dump?:any) {
     if (this.data) {
       this.data.on('selectionChanged.main', null);
     }
@@ -393,8 +359,8 @@ export class LineUp extends utils_.AEventDispatcher {
  * @param columns
  * @returns {model_.IColumnDesc[]}
  */
-export function deriveColors(columns:model_.IColumnDesc[]) {
-  var colors = d3.scale.category10().range().slice();
+export function deriveColors(columns:IColumnDesc[]) {
+  var colors = d3scale.category10().range().slice();
   columns.forEach((col:any) => {
     switch (col.type) {
       case 'number':
@@ -404,19 +370,3 @@ export function deriveColors(columns:model_.IColumnDesc[]) {
   });
   return columns;
 }
-
-/**
- * creates a local storage provider
- * @param data
- * @param columns
- * @param options
- * @returns {LocalDataProvider}
- */
-export function createLocalStorage(data:any[], columns:model_.IColumnDesc[], options = {}) {
-  return new provider_.LocalDataProvider(data, columns, options);
-}
-
-export function create(data:provider_.DataProvider, container:d3.Selection<any> | Element, config:any = {}) {
-  return new LineUp(container, data, config);
-}
-
