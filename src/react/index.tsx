8e6b89aa2b3f4c9cb783bd1f05b5de3e976ee5b8
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-29 11:30:13 +0200

    defaultProps,propTypes as static

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -13,7 +13,7 @@ export interface ILineUpProps<T> {
   desc: IColumnDesc[];
   options?: any;
   selection?: T[];
-  onSelectionChanged?(selection:T[]): void;
+  onSelectionChanged?(selection: T[]): void;
 }
 
 function deepEqual<T>(a: T[], b: T[]) {
@@ -23,10 +23,23 @@ function deepEqual<T>(a: T[], b: T[]) {
   if (a.length !== b.length) {
     return false;
   }
-  return a.every((ai,i) => ai === b[i]);
+  return a.every((ai, i) => ai === b[i]);
 }
 
 export default class LineUp<T> extends React.Component<ILineUpProps<T>, {}> {
+  static propTypes = {
+    data: React.PropTypes.array.isRequired,
+    desc: React.PropTypes.array.isRequired,
+    options: React.PropTypes.any,
+    onSelectionChanged: React.PropTypes.func,
+    selection: React.PropTypes.any
+  };
+
+  static defaultProps = {
+    data: [],
+    desc: []
+  };
+
   private plot: Impl = null;
   private parent: HTMLDivElement = null;
 
@@ -47,7 +60,7 @@ export default class LineUp<T> extends React.Component<ILineUpProps<T>, {}> {
     console.log('create lineup instance');
     const data = new LocalDataProvider(this.props.data, this.props.desc);
     data.on('selectionChanged', this.onSelectionChanged.bind(this));
-    data.selectAll(this.props.selection ? this.props.selection.map((d) => this.item2index(d)): []);
+    data.selectAll(this.props.selection ? this.props.selection.map((d) => this.item2index(d)) : []);
     data.deriveDefault();
     this.plot = new Impl(this.parent, data, this.props);
     this.plot.update();
@@ -64,7 +77,7 @@ export default class LineUp<T> extends React.Component<ILineUpProps<T>, {}> {
   }
 
   componentDidUpdate() {
-    this.plot.data.setSelection(this.props.selection ? this.props.selection.map((d) => this.item2index(d)): []);
+    this.plot.data.setSelection(this.props.selection ? this.props.selection.map((d) => this.item2index(d)) : []);
   }
 
   render() {
@@ -74,18 +87,3 @@ export default class LineUp<T> extends React.Component<ILineUpProps<T>, {}> {
     );
   }
 }
-
-(LineUp as any).propTypes = {
-  data: React.PropTypes.array.isRequired,
-  desc: React.PropTypes.array.isRequired,
-  options: React.PropTypes.any,
-  onSelectionChanged: React.PropTypes.func,
-  selection: React.PropTypes.any
-};
-
-(LineUp as any).defaultProps = {
-  data: [],
-  desc: []
-};
-
-
