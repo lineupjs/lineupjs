3932c4cace9b1d6cc6c3ae8511a55c8b11868655
Bikram Kawan <bikramkawan@gmail.com>
2016-11-24 21:18:18 +0100

    data imported for SVG

--- demo/custom.js
+++ demo/custom.js
@@ -3,32 +3,191 @@
  */
 
 window.onload = function () {
-  var arr = [
-    {a: 10, b: [1,0,1], c: [30,20,10], d: 'Row1', e: false, l: {alt: 'Google', href: 'https://google.com'}, cat: 'c2'},
-    {a: 5, b:  [1,0,1], c: [30,20,10], d: 'Row2', e: true, l: {alt: 'ORF', href: 'https://orf.at'}, cat: 'c3'},
-    {a: 2, b:  [0,1,1], c: [30,20,10], d: 'Row3', e: false, l: {alt: 'heise.de', href: 'https://heise.de'}, cat: 'c2'},
-    {
-      a: 7,
-      b:  [1,0,1],
-      c: [30,20,10],
-      d: 'Row 4',
-      e: false,
-      l: {alt: 'Google', href: 'https://google.com'},
-      cat: 'c1;c3'
-    },
-    {a: 2, b:  [1,1,0], c: [30,20,10], d: 'Row5', e: false, l: {alt: 'heise.de', href: 'https://heise.de'}, cat: 'c2'},
-    {a: NaN, b: [0,0,0], c: [30,20,10], d: 'Row 6', e: false, l: {alt: 'heise.de', href: 'https://heise.de'}, cat: 'c2'}];
-  var desc = [
-    {label: 'Rows', type: 'string', column: 'd', cssClass: 'orange'},
 
-    {label: 'Heatmap', type: 'heatmapcustom', column: 'c', sdomain: [0, 100], colorrange: ['blue', 'white', 'red'], sort: 'min', datalength: 3},
-    {label: 'Sparkline', type: 'sparkline', column: 'c', sdomain: [0, 100], colorrange: ['blue', 'white', 'red'], sort: 'min', datalength: 3},
-    {label: 'Threshold', type: 'threshold', column: 'c', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3},
-    {label: 'Vertical Bar', type: 'verticalbar', column: 'c', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3},
-    {label: 'Box Plot ', type: 'boxplot', column: 'c', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3},
-    {label: 'Upset ', type: 'upset', column: 'b', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3}
 
-  ];
+  function catdata(datapoints) {
+    var temparr = [];
+    for (var i = 0; i < datapoints; i++) {
+
+      //temparr.push('c'+i,i);
+      temparr.push(Math.floor(Math.random() * 2));
+
+    }
+
+    return (temparr);
+  }
+
+ var data;
+  d3.json('data.json', function (data) {
+
+
+    var newdata = [];
+
+
+    data.forEach(function (b) {
+
+      return newdata.push({
+        'country': b.country,
+        'iso': b.iso,
+        'region': b.region,
+        'PI-region': b.piregion,
+        'pi_score': [b.pi_score2012, b.pi_score2013, b.pi_score2014, b.pi_score2015],
+        'eco_score': [b.eco_score2012, b.eco_score2013, b.eco_score2014, b.eco_score2015],
+        'eo_score': [b.eo_score2012, b.eo_score2013, b.eo_score2014, b.eo_score2015],
+        'health_score': [b.health_score2012, b.health_score2013, b.health_score2014, b.health_score2015],
+        'ss_score': [b.ss_score2012, b.ss_score2013, b.ss_score2014, b.ss_score2015],
+        'pf_score': [b.pf_score2012, b.pf_score2013, b.pf_score2014, b.pf_score2015],
+        'sk_score': [b.sk_score2012, b.sk_score2013, b.sk_score2014, b.sk_score2015]
+      });
+
+    })
+
+    var min, max, datalength;
+    newdata.forEach(function (d) {
+
+      var tmp = (d.health_score.map(function (d) {
+        return parseFloat((d));
+      }))
+
+      min = d3.min([min, d3.min(tmp)]);
+      max = d3.max([max, d3.max(tmp)]);
+      datalength = d3.max([datalength, tmp.length])
+
+    })
+    
+    var arr1 = [];
+    newdata.reduce(function (a, b, i) {
+      var arraydata = b.health_score.map(function (x) {
+        return parseFloat(x);
+
+      });
+
+      return arr1.push({
+        country: b.country,
+        heatmapcustom: arraydata,
+        sparkline: arraydata,
+        boxplot: arraydata,
+        threshold: arraydata,
+        verticalbar: arraydata,
+        upset: catdata(4)
+
+      })
+
+    }, 0)
+
+
+    var desc1 = [
+      {label: 'Country', type: 'string', column: 'country'},
+      {
+        label: 'HeatMap',
+        type: 'heatmapcustom',
+        column: 'heatmapcustom',
+        sdomain: [min, max],
+        colorrange: ['blue', 'white', 'red'],
+        sort: 'min',
+        datalength: datalength
+      },
+      {
+        label: 'Spark Line',
+        type: 'sparkline',
+        column: 'sparkline',
+        sdomain: [min, max],
+        sort: 'min',
+        datalength: datalength
+      },
+      {
+        label: 'Box Plot',
+        type: 'boxplot',
+        column: 'boxplot',
+        sdomain: [min, max],
+        sort: 'min',
+        datalength: datalength
+      },
+      {
+        label: 'threshold',
+        type: 'threshold',
+        column: 'threshold',
+        threshold: 0,
+        sort: 'min',
+        colorrange: ['blue', 'red'],
+        datalength: datalength
+      },
+      {
+        label: 'verticalbar',
+        type: 'verticalbar',
+        column: 'verticalbar',
+        sdomain: [min, max],
+        colorrange: ['blue', 'red'],
+        sort: 'min',
+        threshold: 0,
+        datalength: datalength
+      },
+      {
+        label: 'upset',
+        type: 'upset',
+        column: 'upset',
+        sdomain: [min, max],
+        colorrange: ['blue', 'red'],
+        sort: 'countcategory',
+        datalength: datalength
+      }]
+
+
+console.log(desc1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //
+  //
+  // var arr = [
+  //   {a: 10, b: [1,0,1], c: [30,20,10], d: 'Row1', e: false, l: {alt: 'Google', href: 'https://google.com'}, cat: 'c2'},
+  //   {a: 5, b:  [1,0,1], c: [30,20,10], d: 'Row2', e: true, l: {alt: 'ORF', href: 'https://orf.at'}, cat: 'c3'},
+  //   {a: 2, b:  [0,1,1], c: [30,20,10], d: 'Row3', e: false, l: {alt: 'heise.de', href: 'https://heise.de'}, cat: 'c2'},
+  //   {
+  //     a: 7,
+  //     b:  [1,0,1],
+  //     c: [30,20,10],
+  //     d: 'Row 4',
+  //     e: false,
+  //     l: {alt: 'Google', href: 'https://google.com'},
+  //     cat: 'c1;c3'
+  //   },
+  //   {a: 2, b:  [1,1,0], c: [30,20,10], d: 'Row5', e: false, l: {alt: 'heise.de', href: 'https://heise.de'}, cat: 'c2'},
+  //   {a: NaN, b: [0,0,0], c: [30,20,10], d: 'Row 6', e: false, l: {alt: 'heise.de', href: 'https://heise.de'}, cat: 'c2'}];
+
+  // var desc = [
+  //   {label: 'Rows', type: 'string', column: 'd', cssClass: 'orange'},
+  //
+  //   {label: 'Heatmap', type: 'heatmapcustom', column: 'c', sdomain: [0, 100], colorrange: ['blue', 'white', 'red'], sort: 'min', datalength: 3},
+  //   {label: 'Sparkline', type: 'sparkline', column: 'c', sdomain: [0, 100], colorrange: ['blue', 'white', 'red'], sort: 'min', datalength: 3},
+  //   {label: 'Threshold', type: 'threshold', column: 'c', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3},
+  //   {label: 'Vertical Bar', type: 'verticalbar', column: 'c', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3},
+  //   {label: 'Box Plot ', type: 'boxplot', column: 'c', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3},
+  //   {label: 'Upset ', type: 'upset', column: 'b', sdomain: [0, 100], threshold: 20,colorrange: ['blue', 'red'], sort: 'min', datalength: 3}
+  //
+  // ];
   //var colors = d3.scale.category10();
   //desc.forEach(function (d, i) {
   //  d.color = colors(''+i);
@@ -48,18 +207,15 @@ window.onload = function () {
    return p;
    }
    };*/
-  var p = new LineUpJS.provider.LocalDataProvider(arr, desc);
+  var p = new LineUpJS.provider.LocalDataProvider(arr1, desc1);
   var r = p.pushRanking();
 
   var root = d3.select('body');
   r.insert(p.create(LineUpJS.model.createSelectionDesc()), 0);
-  r.push(p.create(desc[0]));
-  r.push(p.create(desc[1]));
-  r.push(p.create(desc[2]));
-  r.push(p.create(desc[3]));
-  r.push(p.create(desc[4]));
-  r.push(p.create(desc[5]));
-  r.push(p.create(desc[6]));
+   desc1.forEach(function (d) {
+     console.log(d)
+      r.push(p.create(d));
+    })
 
   // r.push(p.create(desc[9]));
   // r.push(p.create(desc[10]));
@@ -118,4 +274,5 @@ window.onload = function () {
     }
   });
   body.update();
+    });
 };
