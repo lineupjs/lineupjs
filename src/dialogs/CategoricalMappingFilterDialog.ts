94b29349da7d07e8c12db19ecd15f1bdf00ec039
Christian Lehner <christian.lehner@datavisyn.io>
2017-01-16 10:20:58 +0100

    moved categorical mappings to own class

---
+++ src/dialogs/CategoricalMappingFilterDialog.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by christian on 12.01.17.
+ */
--- src/ui_dialogs.ts
+++ src/ui_dialogs.ts
@@ -126,114 +126,12 @@ export function openEditScriptDialog(column: ScriptColumn, $header: d3.Selection
   });
 }
 
-
-
-
-/**
- * opens the mapping editor for a given CategoricalNumberColumn, i.e. to map categories to numbers
- * @param column the column to rename
- * @param $header the visual header element of this column
- */
-function openCategoricalMappingEditor(column: CategoricalNumberColumn, $header: d3.Selection<any>) {
-  const bak = column.getFilter() || [];
-
-
-  const scale = d3scale.linear().domain([0, 100]).range([0, 120]);
-
-  const $popup = makePopup($header, 'Edit Categorical Mapping', '<div class="selectionTable"><table><thead><th class="selectAll"></th><th colspan="2">Scale</th><th>Category</th></thead><tbody></tbody></table></div>');
-
-  const range = column.getScale().range,
-    colors = column.categoryColors,
-    labels = column.categoryLabels;
-
-  const trData = column.categories.map((d, i) => {
-    return {
-      cat: d,
-      label: labels[i],
-      isChecked: bak.length === 0 || bak.indexOf(d) >= 0,
-      range: range[i] * 100,
-      color: colors[i]
-    };
-  }).sort(sortbyName('label'));
-
-  const $rows = $popup.select('tbody').selectAll('tr').data(trData);
-  const $rowsEnter = $rows.enter().append('tr');
-  $rowsEnter.append('td').attr('class', 'checkmark').on('click', (d) => {
-    d.isChecked = !d.isChecked;
-    redraw();
-  });
-  $rowsEnter.append('td')
-    .append('input').attr({
-    type: 'number',
-    value: (d) => d.range,
-    min: 0,
-    max: 100,
-    size: 5
-  }).on('input', function (d) {
-    d.range = +this.value;
-    redraw();
-  });
-  $rowsEnter.append('td').append('div').attr('class', 'bar').style('background-color', (d) => d.color);
-  $rowsEnter.append('td').attr('class', 'datalabel').text((d) => d.label);
-
-  function redraw() {
-    $rows.select('.checkmark').html((d) => '<i class="fa fa-' + ((d.isChecked) ? 'check-' : '') + 'square-o"></i>');
-    $rows.select('.bar').transition().style('width', (d) => scale(d.range) + 'px');
-    $rows.select('.datalabel').style('opacity', (d) => d.isChecked ? '1.0' : '.8');
-  }
-
-  redraw();
-
-  let isCheckedAll = true;
-
-  function redrawSelectAll() {
-    $popup.select('.selectAll').html((d) => '<i class="fa fa-' + ((isCheckedAll) ? 'check-' : '') + 'square-o"></i>');
-    $popup.select('thead').on('click', () => {
-      isCheckedAll = !isCheckedAll;
-      trData.forEach((row) => row.isChecked = isCheckedAll);
-      redraw();
-      redrawSelectAll();
-    });
-  }
-
-  redrawSelectAll();
-
-  function updateData(filter) {
-    markFiltered($header, filter && filter.length > 0 && filter.length < trData.length);
-    column.setFilter(filter);
-  }
-
-  $popup.select('.cancel').on('click', function () {
-    updateData(bak);
-    column.setMapping(range);
-    $popup.remove();
-  });
-  $popup.select('.reset').on('click', function () {
-    trData.forEach((d) => {
-      d.isChecked = true;
-      d.range = 50;
-    });
-    redraw();
-    updateData(null);
-    column.setMapping(trData.map(() => 1));
-  });
-  $popup.select('.ok').on('click', function () {
-    let f = trData.filter((d) => d.isChecked).map((d) => d.cat);
-    if (f.length === trData.length) {
-      f = [];
-    }
-    updateData(f);
-    column.setMapping(trData.map((d) => d.range / 100));
-    $popup.remove();
-  });
-}
-
 /**
  * returns all known filter dialogs mappings by type
  * @return
  */
 export function filterDialogs() {
   return {
-    ordinal: openCategoricalMappingEditor
+
   };
 }
