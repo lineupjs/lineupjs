78e8d760488e999f2accce1626c3de3f899c30fd
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-06-28 16:02:11 +0200

    first model draft of hierarchy column

---
+++ src/model/HierarchyColumn.ts
@@ -0,0 +1,170 @@
+/**
+ * Created by Samuel Gratzl on 28.06.2017.
+ */
+import {ascending, max, scale} from 'd3';
+import Column from './Column';
+import ValueColumn, {IValueColumnDesc} from './ValueColumn';
+import StringColumn from './StringColumn';
+import {ICategoricalColumn, ICategory} from './CategoricalColumn';
+
+export interface ICategoryNode extends ICategory {
+  readonly children: ICategoryNode[];
+}
+
+export interface IHierarchyDesc extends IValueColumnDesc<string> {
+  readonly hierarchy: ICategoryNode;
+  readonly hiearchySeparator?: string;
+}
+
+export interface ICategoryInternalNode {
+  readonly path: string;
+  readonly name: string;
+  readonly label: string;
+  readonly color: string;
+  readonly children: ICategoryInternalNode[];
+}
+
+/**
+ * column for hierarchical categorical values
+ */
+export default class HierarchyColumn extends ValueColumn<string> implements ICategoricalColumn {
+  static readonly EVENT_CUTOFF_CHANGED = 'cutOffChanged';
+
+  private readonly hierarchySeparator: string;
+  readonly hierarchy: ICategoryInternalNode;
+
+  private currentNode: ICategoryInternalNode;
+  private currentMaxDepth: number = Infinity;
+  private currentLeaves: ICategoryInternalNode[] = [];
+
+  constructor(id: string, desc: IHierarchyDesc) {
+    super(id, desc);
+    this.hierarchySeparator = desc.hiearchySeparator || '.';
+    this.hierarchy = this.initHierarchy(desc.hierarchy);
+    this.currentNode = this.hierarchy;
+    this.currentLeaves = computeLeaves(this.currentNode, this.currentMaxDepth);
+
+    this.setRendererList([
+      {type: 'categorical', label: 'Default'}
+    ]);
+  }
+
+  initHierarchy(root: ICategoryNode) {
+    const colors = scale.category10().range().slice();
+    const s = this.hierarchySeparator;
+    const add = (prefix: string, node: ICategoryNode): ICategoryInternalNode => {
+      const name = node.name || node.value;
+      let lastColorUsed = -1;
+      const children = (node.children || []).map((child: ICategoryNode|string): ICategoryInternalNode => {
+        if (typeof child === 'string') {
+          return {
+            path: prefix + child,
+            name: child,
+            label: child,
+            color: colors[(lastColorUsed++) % colors.length],
+            children: []
+          };
+        }
+        const r = add(`${prefix}${name}${s}`, child);
+        if (!r.color) {
+          //hack to inject the next color
+          (<any>r).color = colors[(lastColorUsed++) % colors.length];
+        }
+        return r;
+      });
+      return {path: prefix + name, name, children, label: node.label || name, color: node.color};
+    };
+    return add('', root);
+  }
+
+  protected createEventList() {
+    return super.createEventList().concat([HierarchyColumn.EVENT_CUTOFF_CHANGED]);
+  }
+
+  get categories() {
+    return this.currentLeaves.map((c) => c.name);
+  }
+
+  get categoryLabels() {
+    return this.currentLeaves.map((c) => c.label);
+  }
+
+  getCutOff() {
+    return {
+      node: this.currentNode,
+      maxDepth: this.currentMaxDepth
+    }
+  }
+
+  setCutOff(node: ICategoryInternalNode, maxDepth: number = Infinity) {
+    if (this.currentNode === node && this.currentMaxDepth === maxDepth) {
+      return;
+    }
+    const bak = this.getCutOff();
+    this.currentNode = node;
+    this.currentMaxDepth = maxDepth;
+    this.currentLeaves = computeLeaves(node, maxDepth);
+    this.fire([HierarchyColumn.EVENT_CUTOFF_CHANGED, Column.EVENT_DIRTY_VALUES, Column.EVENT_DIRTY], bak, this.getCutOff());
+  }
+
+  private resolveCategory(row: any, index: number) {
+    const base = StringColumn.prototype.getValue.call(this, row, index);
+    if (base === null) {
+      return null;
+    }
+    const node = this.currentLeaves.find((n) => {
+      //direct hit or is a child of it
+      return n.path === base || base.startsWith(n.path + this.hierarchySeparator);
+    });
+    //unify to null
+    return node || null;
+  }
+
+  getValue(row: any, index: number) {
+    const base = this.resolveCategory(row, index);
+    return base ? base.name : null;
+  }
+
+  getLabel(row: any, index: number) {
+    const base = this.resolveCategory(row, index);
+    if (!base) {
+      return '';
+    }
+    if (base.label === base.name) {
+      return base.path;
+    }
+    return `${base.path}: ${base.label}`;
+  }
+
+  getCategories(row: any, index: number) {
+    const v = this.getValue(row, index);
+    return v === null ? [] : [v];
+  }
+
+  getColor(row: any, index: number) {
+    const base = this.resolveCategory(row, index);
+    return base ? base.color : null;
+  }
+
+  compare(a: any, b: any, aIndex: number, bIndex: number) {
+    const va = this.getValue(a, aIndex);
+    const vb = this.getValue(b, bIndex);
+    return ascending(va, vb);
+  }
+}
+
+function computeLeaves(node: ICategoryInternalNode, maxDepth: number = Infinity) {
+  const leaves: ICategoryInternalNode[] = [];
+  //depth first
+  const visit = (node: ICategoryInternalNode, depth: number) => {
+    //hit or end
+    if (depth >= maxDepth || node.children.length === 0) {
+      leaves.push(node);
+    } else {
+      // go down
+      node.children.forEach((c) => visit(c, depth + 1));
+    }
+  };
+  visit(node, 0);
+  return leaves;
+}
