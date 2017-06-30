bf477bea0812366508a131674bb9deebb804156d
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-06-30 14:20:37 +0200

    extract math function to utils

--- src/provider/LocalDataProvider.ts
+++ src/provider/LocalDataProvider.ts
@@ -2,80 +2,15 @@
  * Created by sam on 04.11.2016.
  */
 
-import Column, {IStatistics, ICategoricalStatistics, IColumnDesc} from '../model/Column';
+import Column, {IColumnDesc} from '../model/Column';
 import NumberColumn, {INumberColumn} from '../model/NumberColumn';
 import Ranking from '../model/Ranking';
 import {ICategoricalColumn} from '../model/CategoricalColumn';
-import {merge} from '../utils';
+import {merge, computeHist, computeStats} from '../utils';
 import * as d3 from 'd3';
 import {IStatsBuilder, IDataProviderOptions, IDataRow} from './ADataProvider';
 import ACommonDataProvider from './ACommonDataProvider';
 import {IOrderedGroup} from '../model/Group';
-
-/**
- * computes the simple statistics of an array using d3 histogram
- * @param arr the data array
- * @param indices array data indices
- * @param acc accessor function
- * @param range the total value range
- * @returns {{min: number, max: number, count: number, hist: histogram.Bin<number>[]}}
- */
-function computeStats(arr: any[], indices: number[], acc: (row: any, index: number) => number, range?: [number, number]): IStatistics {
-  if (arr.length === 0) {
-    return {
-      min: NaN,
-      max: NaN,
-      mean: NaN,
-      count: 0,
-      maxBin: 0,
-      hist: []
-    };
-  }
-  const indexAccessor = (a, i) => acc(a, indices[i]);
-  const hist = d3.layout.histogram().value(indexAccessor);
-  if (range) {
-    hist.range(() => range);
-  }
-  const ex = d3.extent(arr, indexAccessor);
-  const histData = hist(arr);
-  return {
-    min: ex[0],
-    max: ex[1],
-    mean: d3.mean(arr, indexAccessor),
-    count: arr.length,
-    maxBin: d3.max(histData, (d) => d.y),
-    hist: histData
-  };
-}
-
-/**
- * computes a categorical histogram
- * @param arr the data array
- * @param indices the data array data indices
- * @param acc the accessor
- * @param categories the list of known categories
- * @returns {{hist: {cat: string, y: number}[]}}
- */
-function computeHist(arr: number[], indices: number[], acc: (row: any, index: number) => string[], categories: string[]): ICategoricalStatistics {
-  const m = new Map<string,number>();
-  categories.forEach((cat) => m.set(cat, 0));
-
-  arr.forEach((a, i) => {
-    const vs = acc(a, indices[i]);
-    if (vs == null) {
-      return;
-    }
-    vs.forEach((v) => {
-      m.set(v, (m.get(v) || 0) + 1);
-    });
-  });
-  const entries: {cat: string; y: number}[] = [];
-  m.forEach((v, k) => entries.push({cat: k, y: v}));
-  return {
-    maxBin: Math.max(...entries.map((d) => d.y)),
-    hist: entries
-  };
-}
 
 export interface ILocalDataProviderOptions {
   /**
--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,8 @@
  * Created by Samuel Gratzl on 14.08.2015.
  */
 
-import {dispatch, select, event as d3event, Dispatch} from 'd3';
-import Column from './model/Column';
+import {dispatch, select, event as d3event, Dispatch, mean, layout, extent} from 'd3';
+import Column, {ICategoricalStatistics, IStatistics} from './model/Column';
 import {IDOMCellRenderer, IDOMGroupRenderer} from './renderer/IDOMCellRenderers';
 
 /**
@@ -544,4 +544,70 @@ export function equalArrays<T>(a: T[], b: T[]) {
     return false;
   }
   return a.every((ai, i) => ai === b[i]);
+}
+
+
+/**
+ * computes the simple statistics of an array using d3 histogram
+ * @param arr the data array
+ * @param indices array data indices
+ * @param acc accessor function
+ * @param range the total value range
+ * @returns {{min: number, max: number, count: number, hist: histogram.Bin<number>[]}}
+ */
+export function computeStats(arr: any[], indices: number[], acc: (row: any, index: number) => number, range?: [number, number]): IStatistics {
+  if (arr.length === 0) {
+    return {
+      min: NaN,
+      max: NaN,
+      mean: NaN,
+      count: 0,
+      maxBin: 0,
+      hist: []
+    };
+  }
+  const indexAccessor = (a, i) => acc(a, indices[i]);
+  const hist = layout.histogram().value(indexAccessor);
+  if (range) {
+    hist.range(() => range);
+  }
+  const ex = extent(arr, indexAccessor);
+  const histData = hist(arr);
+  return {
+    min: ex[0],
+    max: ex[1],
+    mean: mean(arr, indexAccessor),
+    count: arr.length,
+    maxBin: Math.max(...histData.map((d) => d.y)),
+    hist: histData
+  };
+}
+
+/**
+ * computes a categorical histogram
+ * @param arr the data array
+ * @param indices the data array data indices
+ * @param acc the accessor
+ * @param categories the list of known categories
+ * @returns {{hist: {cat: string, y: number}[]}}
+ */
+export function computeHist(arr: number[], indices: number[], acc: (row: any, index: number) => string[], categories: string[]): ICategoricalStatistics {
+  const m = new Map<string,number>();
+  categories.forEach((cat) => m.set(cat, 0));
+
+  arr.forEach((a, i) => {
+    const vs = acc(a, indices[i]);
+    if (vs == null) {
+      return;
+    }
+    vs.forEach((v) => {
+      m.set(v, (m.get(v) || 0) + 1);
+    });
+  });
+  const entries: {cat: string; y: number}[] = [];
+  m.forEach((v, k) => entries.push({cat: k, y: v}));
+  return {
+    maxBin: Math.max(...entries.map((d) => d.y)),
+    hist: entries
+  };
 }
