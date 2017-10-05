5e9e74c2d0668dbbf331cec4bb613c03b5beeaf4
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-10-05 15:21:08 +0200

    fix script order

--- src/model/ScriptColumn.ts
+++ src/model/ScriptColumn.ts
@@ -55,82 +55,6 @@ function wrapWithContext(code: string) {
   return typeof v === 'number' ? v : NaN`;
 }
 
-export interface IScriptDesc extends ICompositeNumberDesc {
-  /**
-   * the function to use, it has two parameters: children (current children) and values (their row values)
-   * @default 'return Math.max.apply(Math,values)'
-   */
-  script?: string;
-}
-
-export declare type IScriptColumnDesc = IScriptDesc & ICompositeNumberDesc;
-
-
-export default class ScriptColumn extends CompositeNumberColumn {
-  static readonly EVENT_SCRIPT_CHANGED = 'scriptChanged';
-  static readonly DEFAULT_SCRIPT = DEFAULT_SCRIPT;
-
-  private script = ScriptColumn.DEFAULT_SCRIPT;
-  private f: Function | null = null;
-
-  constructor(id: string, desc: IScriptColumnDesc) {
-    super(id, desc);
-    this.script = desc.script || this.script;
-  }
-
-  protected createEventList() {
-    return super.createEventList().concat([ScriptColumn.EVENT_SCRIPT_CHANGED]);
-  }
-
-  setScript(script: string) {
-    if (this.script === script) {
-      return;
-    }
-    this.f = null;
-    this.fire([ScriptColumn.EVENT_SCRIPT_CHANGED, Column.EVENT_DIRTY_VALUES, Column.EVENT_DIRTY], this.script, this.script = script);
-  }
-
-  getScript() {
-    return this.script;
-  }
-
-  dump(toDescRef: (desc: any) => any) {
-    const r = super.dump(toDescRef);
-    r.script = this.script;
-    return r;
-  }
-
-  restore(dump: any, factory: (dump: any) => Column | null) {
-    this.script = dump.script || this.script;
-    super.restore(dump, factory);
-  }
-
-  protected compute(row: any, index: number) {
-    if (this.f == null) {
-      this.f = new Function('children', 'values', 'raws', 'col', 'row', 'index', wrapWithContext(this.script));
-    }
-    const children = this._children;
-    const values = this._children.map((d) => d.getValue(row, index));
-    const raws = <number[]>this._children.map((d) => isNumberColumn(d) ? d.getRawNumber(row, index) : null);
-    const col = new ColumnContext(children.map((c, i) => new ColumnWrapper(c, values[i], raws[i])), () => {
-      const cols = this.findMyRanker()!.flatColumns;
-      return new ColumnContext(cols.map((c) =>  new ColumnWrapper(c, c.getValue(row, index), isNumberColumn(c) ? c.getRawNumber(row, index): null)));
-    });
-    return this.f.call(this, children, values, raws, col, row, index);
-  }
-
-  /**
-   * describe the column if it is a sorting criteria
-   * @param toId helper to convert a description to an id
-   * @return {string} json compatible
-   */
-  toSortingDesc(toId: (desc: any) => string): any {
-    return {
-      code: this.script,
-      operands: this._children.map((c) => c.toSortingDesc(toId))
-    };
-  }
-}
 
 /**
  * wrapper class for simpler column accessing
@@ -215,5 +139,83 @@ class ColumnContext {
       this._all = this.allFactory ? this.allFactory() : null;
     }
     return this._all!;
+  }
+}
+
+
+export interface IScriptDesc extends ICompositeNumberDesc {
+  /**
+   * the function to use, it has two parameters: children (current children) and values (their row values)
+   * @default 'return Math.max.apply(Math,values)'
+   */
+  script?: string;
+}
+
+export declare type IScriptColumnDesc = IScriptDesc & ICompositeNumberDesc;
+
+
+export default class ScriptColumn extends CompositeNumberColumn {
+  static readonly EVENT_SCRIPT_CHANGED = 'scriptChanged';
+  static readonly DEFAULT_SCRIPT = DEFAULT_SCRIPT;
+
+  private script = ScriptColumn.DEFAULT_SCRIPT;
+  private f: Function | null = null;
+
+  constructor(id: string, desc: IScriptColumnDesc) {
+    super(id, desc);
+    this.script = desc.script || this.script;
+  }
+
+  protected createEventList() {
+    return super.createEventList().concat([ScriptColumn.EVENT_SCRIPT_CHANGED]);
+  }
+
+  setScript(script: string) {
+    if (this.script === script) {
+      return;
+    }
+    this.f = null;
+    this.fire([ScriptColumn.EVENT_SCRIPT_CHANGED, Column.EVENT_DIRTY_VALUES, Column.EVENT_DIRTY], this.script, this.script = script);
+  }
+
+  getScript() {
+    return this.script;
+  }
+
+  dump(toDescRef: (desc: any) => any) {
+    const r = super.dump(toDescRef);
+    r.script = this.script;
+    return r;
+  }
+
+  restore(dump: any, factory: (dump: any) => Column | null) {
+    this.script = dump.script || this.script;
+    super.restore(dump, factory);
+  }
+
+  protected compute(row: any, index: number) {
+    if (this.f == null) {
+      this.f = new Function('children', 'values', 'raws', 'col', 'row', 'index', wrapWithContext(this.script));
+    }
+    const children = this._children;
+    const values = this._children.map((d) => d.getValue(row, index));
+    const raws = <number[]>this._children.map((d) => isNumberColumn(d) ? d.getRawNumber(row, index) : null);
+    const col = new ColumnContext(children.map((c, i) => new ColumnWrapper(c, values[i], raws[i])), () => {
+      const cols = this.findMyRanker()!.flatColumns;
+      return new ColumnContext(cols.map((c) =>  new ColumnWrapper(c, c.getValue(row, index), isNumberColumn(c) ? c.getRawNumber(row, index): null)));
+    });
+    return this.f.call(this, children, values, raws, col, row, index);
+  }
+
+  /**
+   * describe the column if it is a sorting criteria
+   * @param toId helper to convert a description to an id
+   * @return {string} json compatible
+   */
+  toSortingDesc(toId: (desc: any) => string): any {
+    return {
+      code: this.script,
+      operands: this._children.map((c) => c.toSortingDesc(toId))
+    };
   }
 }
