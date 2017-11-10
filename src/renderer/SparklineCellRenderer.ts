cca2b32d797808bc2b5dabeab38682ebda5567a8
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-11-10 17:52:20 +0100

    improve spark line renderer vor missing values

--- src/renderer/SparklineCellRenderer.ts
+++ src/renderer/SparklineCellRenderer.ts
@@ -8,13 +8,27 @@ import Column from '../model/Column';
 import {matchRows} from './ANumbersCellRenderer';
 import {forEachChild} from '../utils';
 import {IGroup} from '../model/Group';
+import {renderMissingCanvas, renderMissingDOM} from './missing';
+import {isMissingValue} from '../model/missing';
 
 export function line(data: number[]) {
   if (data.length === 0) {
     return '';
   }
-  const first = data[0];
-  return `M0,${1 - first} ${data.slice(1).map((d, i) => `L${i + 1},${1 - d}`).join(' ')}`;
+  let p = '';
+  let moveNext = true;
+
+  data.forEach((d, i) => {
+    if (isMissingValue(d)) {
+      moveNext = true;
+    } else if (moveNext) {
+      p += `M${i},${1 - d} `;
+      moveNext = false;
+    } else {
+      p += `L${i},${1 - d} `;
+    }
+  });
+  return p;
 }
 
 export default class SparklineCellRenderer implements ICellRendererFactory {
@@ -24,26 +38,29 @@ export default class SparklineCellRenderer implements ICellRendererFactory {
     return {
       template: `<svg viewBox="0 0 ${col.getDataLength() - 1} 1" preserveAspectRatio="none meet"><line x1="0" x2="${col.getDataLength() - 1}" y1="${yPos}" y2="${yPos}"></line><path></path></svg>`,
       update: (n: HTMLElement, d: IDataRow) => {
-        const data = col.getNumbers(d.v, d.dataIndex);
-        if(data.some((v) => isNaN(v))) {
-          n.querySelector('path')!.setAttribute('d', '');
-        } else {
-          n.querySelector('path')!.setAttribute('d', line(data));
+        if (renderMissingDOM(n, col, d)) {
+          return;
         }
+        const data = col.getNumbers(d.v, d.dataIndex);
+        n.querySelector('path')!.setAttribute('d', line(data));
       }
     };
   }
 
   createCanvas(col: INumbersColumn & Column, context: ICanvasRenderContext): ICanvasCellRenderer {
     return (ctx: CanvasRenderingContext2D, d: IDataRow, i: number) => {
+      const h = context.rowHeight(i);
+      if (renderMissingCanvas(ctx, col, d, h)) {
+        return;
+      }
       const data = col.getNumbers(d.v, d.dataIndex);
       if (data.length === 0) {
         return;
       }
       ctx.save();
       const w = context.colWidth(col) / (col.getDataLength()-1);
-      const h = context.rowHeight(i);
 
+      // base line
       ctx.strokeStyle = '#c1c1c1';
       ctx.beginPath();
       ctx.moveTo(0, 1 - col.getMapping().apply(col.getThreshold()));
@@ -51,14 +68,31 @@ export default class SparklineCellRenderer implements ICellRendererFactory {
       ctx.stroke();
 
       ctx.strokeStyle = 'black';
-      ctx.beginPath();
-      ctx.moveTo(0, (1 - data[0]) * h);
-      for (let i = 1; i < data.length; ++i) {
-        ctx.lineTo(i * w, (1 - data[i]) * h);
-      }
-      ctx.stroke();
+      this.renderLine(ctx, data, h, w);
       ctx.restore();
     };
+  }
+
+  private renderLine(ctx: CanvasRenderingContext2D, data: number[], w: number, h: number) {
+    ctx.beginPath();
+    let moveNext = false;
+    if (isMissingValue(data[0])) {
+      moveNext = true;
+    } else {
+      ctx.moveTo(0, (1 - data[0]) * h);
+    }
+    for (let i = 1; i < data.length; ++i) {
+      const v = data[i];
+      if (isMissingValue(v)) {
+        moveNext = true;
+      } else if (moveNext) {
+        ctx.moveTo(i * w, (1 - data[i]) * h);
+        moveNext = false;
+      } else {
+        ctx.lineTo(i * w, (1 - data[i]) * h);
+      }
+    }
+    ctx.stroke();
   }
 
   createGroupDOM(col: INumbersColumn & Column): IDOMGroupRenderer {
@@ -79,14 +113,15 @@ export default class SparklineCellRenderer implements ICellRendererFactory {
   createGroupCanvas(col: INumbersColumn & Column, context: ICanvasRenderContext): ICanvasGroupRenderer {
     return (ctx: CanvasRenderingContext2D, group: IGroup, rows: IDataRow[]) => {
       //overlapping ones
-      const groupHeight = context.groupHeight(group);
+      const h = context.groupHeight(group);
+      const w = context.colWidth(col);
       ctx.save();
-      ctx.scale(context.colWidth(col) / col.getDataLength(), groupHeight);
 
       ctx.strokeStyle = '#c1c1c1';
       ctx.beginPath();
-      ctx.moveTo(0, 1 - col.getMapping().apply(col.getThreshold()));
-      ctx.lineTo(context.colWidth(col) / col.getDataLength(), groupHeight);
+      const tresholdLine = (1 - col.getMapping().apply(col.getThreshold())) * h;
+      ctx.moveTo(0, tresholdLine);
+      ctx.lineTo(w, tresholdLine);
       ctx.stroke();
 
       ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
@@ -96,12 +131,7 @@ export default class SparklineCellRenderer implements ICellRendererFactory {
         if (data.length === 0) {
           return;
         }
-        ctx.beginPath();
-        ctx.moveTo(0, 1 - data[0]);
-        for (let i = 1; i < data.length; ++i) {
-          ctx.lineTo(i, 1 - data[i]);
-        }
-        ctx.stroke();
+        this.renderLine(ctx, data, w, h);
       });
       ctx.restore();
     };
