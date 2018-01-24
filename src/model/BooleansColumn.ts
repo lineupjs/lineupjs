3d65876697390073b865787ea62c51399d782600
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-01-24 22:19:01 +0100

    fix boolean columns column being a set column

--- src/model/BooleansColumn.ts
+++ src/model/BooleansColumn.ts
@@ -1,13 +1,23 @@
 import ArrayColumn, {IArrayColumnDesc} from './ArrayColumn';
+import {ISetColumn, toCategory} from './ICategoricalColumn';
 import {IDataRow} from './interfaces';
 import {FIRST_IS_NAN} from './missing';
 
 export declare type IBooleansColumnDesc = IArrayColumnDesc<boolean>;
 
-export default class BooleansColumn extends ArrayColumn<boolean> {
+export default class BooleansColumn extends ArrayColumn<boolean> implements ISetColumn {
   constructor(id: string, desc: Readonly<IBooleansColumnDesc>) {
     super(id, desc);
     this.setDefaultRenderer('upset');
+  }
+
+  get categories() {
+    return this.labels.map((d, i) => toCategory(d, i));
+  }
+
+  getSet(row: IDataRow) {
+    const vs = this.getValues(row);
+    return new Set(this.categories.filter((_, i) => vs[i]));
   }
 
   compare(a: IDataRow, b: IDataRow) {
