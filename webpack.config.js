65f94017e6d360a893f834fec37a595de78cc0d0
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-04 19:43:22 +0100

    downgrade till plugins are ready

--- webpack.config.js
+++ webpack.config.js
@@ -105,14 +105,10 @@ function generateWebpack(options) {
   };
 
   if (options.isProduction) {
-      base.plugins.unshift(new webpack.BannerPlugin({
-        banner: banner,
-        raw: true
-      }));
-      base.plugins.push(new webpack.optimize.MinChunkSizePlugin({
-        minChunkSize: 10000 //at least 10.000 characters
-      }),
-      new webpack.optimize.AggressiveMergingPlugin());
+    base.plugins.unshift(new webpack.BannerPlugin({
+      banner: banner,
+      raw: true
+    }));
   }
 
   if (!options.isTest) {
