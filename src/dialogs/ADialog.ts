0a34ab1e1d9bcbeb6e3843970424305037ac63e2
sluger <stefan.luger@datavisyn.io>
2018-02-05 12:58:52 +0100

    reformat code

--- src/dialogs/ADialog.ts
+++ src/dialogs/ADialog.ts
@@ -2,23 +2,23 @@ import Popper from 'popper.js';
 
 class DialogStack {
 
-  readonly openDialogs:ADialog[] = [];
+  readonly openDialogs: ADialog[] = [];
 
-  private backdrop:HTMLElement;
+  private backdrop: HTMLElement;
 
   private backdropListener = () => {
     this.removeAll();
   };
 
-  private escKeyListener = (evt:KeyboardEvent) => {
+  private escKeyListener = (evt: KeyboardEvent) => {
     if (evt.which === 27 && this.openDialogs.length > 0) {
       this.removeLast();
     }
   };
 
-  add(dialog:ADialog):boolean {
+  add(dialog: ADialog): boolean {
     // add esc listener and backdrop for first dialog
-    if(this.openDialogs.length === 0) {
+    if (this.openDialogs.length === 0) {
       dialog.attachment.ownerDocument.addEventListener('keyup', this.escKeyListener);
 
       dialog.attachment.ownerDocument.body.insertAdjacentHTML('beforeend', `<div class="lu-backdrop"><div class="lu-backdrop-mask"></div></div>`);
@@ -26,7 +26,7 @@ class DialogStack {
       this.backdrop.addEventListener('click', this.backdropListener);
 
       const mask = dialog.backdropMaskRect();
-      if(mask) {
+      if (mask) {
         // @see http://bennettfeely.com/clippy/ -> select `Frame` example
         (<HTMLElement>this.backdrop.querySelector('.lu-backdrop-mask'))!.style.clipPath = `polygon(
           0% 0%,
@@ -42,13 +42,13 @@ class DialogStack {
         )`;
       }
 
-    // check for every further dialog if the same dialog is already open -> if yes, close it == toggle behavior
-    } else if(this.openDialogs[this.openDialogs.length - 1].constructor === dialog.constructor) {
+      // check for every further dialog if the same dialog is already open -> if yes, close it == toggle behavior
+    } else if (this.openDialogs[this.openDialogs.length - 1].constructor === dialog.constructor) {
       this.removeLast();
       return false;
 
-    // if dialogs are not a menu, close previous one before opening the new dialog
-    } else if(!this.openDialogs[this.openDialogs.length - 1].isMenuDialog && !dialog.isMenuDialog) {
+      // if dialogs are not a menu, close previous one before opening the new dialog
+    } else if (!this.openDialogs[this.openDialogs.length - 1].isMenuDialog && !dialog.isMenuDialog) {
       this.removeLast();
     }
 
@@ -56,14 +56,14 @@ class DialogStack {
     return true;
   }
 
-  remove(dialog:ADialog) {
+  remove(dialog: ADialog) {
     const index = this.openDialogs.indexOf(dialog);
-    if(index > -1 && dialog) {
+    if (index > -1 && dialog) {
       this.openDialogs.splice(index, 1);
       dialog.close(false);
     }
 
-    if(this.openDialogs.length > 0) {
+    if (this.openDialogs.length > 0) {
       return;
     }
 
@@ -77,7 +77,7 @@ class DialogStack {
   }
 
   removeAll() {
-    if(this.openDialogs.length === 0) {
+    if (this.openDialogs.length === 0) {
       return;
     }
 
@@ -99,9 +99,9 @@ export interface IMaskRect {
 
 abstract class ADialog {
 
-  public isMenuDialog:boolean = false;
+  public isMenuDialog: boolean = false;
 
-  public backdropMaskRect:() => IMaskRect;
+  public backdropMaskRect: () => IMaskRect;
 
   public node: HTMLElement;
 
@@ -111,10 +111,10 @@ abstract class ADialog {
 
   }
 
-  protected abstract build():HTMLElement;
+  protected abstract build(): HTMLElement;
 
   open(): void {
-    if(!dialogStack.add(this)) {
+    if (!dialogStack.add(this)) {
       return; // not added because of toggle behavior
     }
 
@@ -130,10 +130,10 @@ abstract class ADialog {
     });
   }
 
-  close(removeFromStack:boolean = true): void {
+  close(removeFromStack: boolean = true): void {
     this.popper.destroy();
 
-    if(removeFromStack) {
+    if (removeFromStack) {
       dialogStack.remove(this);
     }
   }
@@ -144,7 +144,7 @@ abstract class ADialog {
    * @param body
    * @returns {HTMLElement}
    */
-  protected makeMenuPopup(body: string):HTMLElement {
+  protected makeMenuPopup(body: string): HTMLElement {
     const parent = this.attachment.ownerDocument.body;
     parent.insertAdjacentHTML('beforeend', `<div class="lu-popup2 lu-popup-menu">${body}</div>`);
     return <HTMLElement>parent.lastElementChild!;
@@ -155,7 +155,7 @@ abstract class ADialog {
    * @param body
    * @returns {HTMLElement}
    */
-  protected makePopup(body: string):HTMLElement {
+  protected makePopup(body: string): HTMLElement {
     const parent = this.attachment.ownerDocument.body;
     parent.insertAdjacentHTML('beforeend', `<div class="lu-popup2">${this.dialogForm(body)}</div>`);
     const popup = <HTMLElement>parent.lastElementChild!;
@@ -168,7 +168,7 @@ abstract class ADialog {
     return popup;
   }
 
-  protected makeChoosePopup(body: string):HTMLElement {
+  protected makeChoosePopup(body: string): HTMLElement {
     const parent = this.attachment.ownerDocument.body;
     parent.insertAdjacentHTML('beforeend', `<div class="lu-popup2 chooser">${this.basicDialog(body)}</div>`);
     return <HTMLElement>parent.lastElementChild!;
@@ -185,7 +185,7 @@ abstract class ADialog {
             </form>`;
   }
 
-  protected onButton(node:HTMLElement, handler: { submit: () => boolean, reset: () => void, cancel: () => void }) {
+  protected onButton(node: HTMLElement, handler: { submit: () => boolean, reset: () => void, cancel: () => void }) {
     node.querySelector('.cancel')!.addEventListener('click', (evt) => {
       handler.cancel();
       dialogStack.remove(this);
