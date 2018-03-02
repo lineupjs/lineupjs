cb64b2a2a2dbce4aad22f24ae2819ad3b12a0afb
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-02 21:46:40 +0100

    try to avoid thread-loader in CI

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,29 @@ const banner = '/*! ' + (pkg.title || pkg.name) + ' - v' + pkg.version + ' - ' +
   '* Copyright (c) ' + year + ' ' + pkg.author.name + ';' +
   ' Licensed ' + pkg.license + '*/\n';
 
+const tsLoader = [{
+    loader: 'cache-loader'
+  },
+  {
+    loader: 'thread-loader',
+    options: {
+     // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+     workers: require('os').cpus().length - 1,
+    },
+  },
+  {
+    loader: 'ts-loader',
+    options: {
+      configFile: env === 'dev' ? 'tsconfig_dev.json' : 'tsconfig.json',
+      happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up  compilation and reduce errors reported to webpack
+    }
+  }
+];
+
+if (process.env.CI) {
+  tsLoader.splice(0, 2); // just the loader no optimization
+}
+
 /**
  * generate a webpack configuration
  */
@@ -67,24 +90,7 @@ module.exports = (env) => {
         },
         {
           test: /\.tsx?$/,
-          use: [{
-              loader: 'cache-loader'
-            },
-            {
-              loader: 'thread-loader',
-              options: {
-                // there should be 1 cpu for the fork-ts-checker-webpack-plugin
-                workers: require('os').cpus().length - 1,
-              },
-            },
-            {
-              loader: 'ts-loader',
-              options: {
-                configFile: env === 'dev' ? 'tsconfig_dev.json' : 'tsconfig.json',
-                happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
-              }
-            }
-          ]
+          use: tsLoader
         },
         {
           test: /\.(png|jpg)$/,
