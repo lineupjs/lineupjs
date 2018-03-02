03e9fa19801fd60208dfb37fafb9a1a89b29ddfc
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-02 22:15:52 +0100

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -16,33 +16,34 @@ const banner = '/*! ' + (pkg.title || pkg.name) + ' - v' + pkg.version + ' - ' +
   '* Copyright (c) ' + year + ' ' + pkg.author.name + ';' +
   ' Licensed ' + pkg.license + '*/\n';
 
-const tsLoader = [{
-    loader: 'cache-loader'
-  },
-  {
-    loader: 'thread-loader',
-    options: {
-     // there should be 1 cpu for the fork-ts-checker-webpack-plugin
-     workers: require('os').cpus().length - 1,
-    },
-  },
-  {
-    loader: 'ts-loader',
-    options: {
-      configFile: env === 'dev' ? 'tsconfig_dev.json' : 'tsconfig.json',
-      happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up  compilation and reduce errors reported to webpack
-    }
-  }
-];
-
-if (process.env.CI) {
-  tsLoader.splice(0, 2); // just the loader no optimization
-}
-
 /**
  * generate a webpack configuration
  */
 module.exports = (env) => {
+
+  const tsLoader = [{
+      loader: 'cache-loader'
+    },
+    {
+      loader: 'thread-loader',
+      options: {
+        // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+        workers: require('os').cpus().length - 1,
+      },
+    },
+    {
+      loader: 'ts-loader',
+      options: {
+        configFile: env === 'dev' ? 'tsconfig_dev.json' : 'tsconfig.json',
+        happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up  compilation and reduce errors reported to webpack
+      }
+    }
+  ];
+
+  if (process.env.CI) {
+    tsLoader.splice(0, 2); // just the loader no optimization
+  }
+
   return {
     entry: {
       'LineUpJS': './src/index.ts'
