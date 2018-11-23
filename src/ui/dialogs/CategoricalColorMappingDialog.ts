31bade4c1ad2c828436010669fb05e2ca8c20f06
Samuel Gratzl <sam@sgratzl.com>
2018-11-23 16:04:28 +0100

    add chooser for color scheme

--- src/ui/dialogs/CategoricalColorMappingDialog.ts
+++ src/ui/dialogs/CategoricalColorMappingDialog.ts
@@ -6,6 +6,9 @@ import { DEFAULT_COLOR_FUNCTION, ReplacmentColorMappingFunction} from '../../mod
 import CategoricalsColumn from '../../model/CategoricalsColumn';
 import CategoricalMapColumn from '../../model/CategoricalMapColumn';
 import {color} from 'd3-color';
+import {schemeCategory10, schemeAccent, schemeDark2, schemePastel1, schemePastel2, schemeSet1, schemeSet2, schemeSet3} from 'd3-scale-chromatic';
+
+const sets: {[key: string]: ReadonlyArray<string>} = {schemeCategory10, schemeAccent, schemeDark2, schemePastel1, schemePastel2, schemeSet1, schemeSet2, schemeSet3};
 
 /** @internal */
 export default class CategoricalColorMappingDialog extends ADialog {
@@ -20,12 +23,35 @@ export default class CategoricalColorMappingDialog extends ADialog {
     const id = uniqueId(this.dialog.idPrefix);
     const mapping = this.column.getColorMapping();
     node.insertAdjacentHTML('beforeend', `<div class="${cssClass('dialog-table')}">
-        ${this.column.categories.map((d) => `
-          <div class="${cssClass('dialog-color-table-entry')}">
-            <input id="${id}${d.name}" data-cat="${d.name}" type="color" value="${color(mapping.apply(d))!.hex()}">
-            <label for="${id}${d.name}">${d.label}</label>
-          </div>`).join('')}
+      <div class="${cssClass('dialog-color-table-entry')}">
+        <select id="${id}Chooser">
+          <option value="">Apply Color Scheme...</option>
+          <option value="schemeCategory10">D3 Category 10 (${schemeCategory10.length})</option>
+          <option value="schemeSet1">Set 1 (${schemeSet1.length})</option>
+          <option value="schemeSet2">Set 2 (${schemeSet2.length})</option>
+          <option value="schemeSet3">Set 3 (${schemeSet3.length})</option>
+          <option value="schemeAccent">Accent (${schemeAccent.length})</option>
+          <option value="schemeDark2">Dark2 (${schemeDark2.length})</option>
+          <option value="schemePastel1">Pastel 1 (${schemePastel1.length})</option>
+          <option value="schemePastel2">Pastel 2 (${schemePastel2.length})</option>
+        </select>
+      </div>
+      ${this.column.categories.map((d) => `
+        <div class="${cssClass('dialog-color-table-entry')}">
+          <input id="${id}${d.name}" data-cat="${d.name}" type="color" value="${color(mapping.apply(d))!.hex()}">
+          <label for="${id}${d.name}">${d.label}</label>
+        </div>`).join('')}
     </div>`);
+
+    this.findInput('select').onchange = (evt) => {
+      const scheme = sets[(<HTMLInputElement>evt.currentTarget).value];
+      if (!scheme) {
+        return;
+      }
+      this.forEach('[data-cat]', (n: HTMLInputElement, i) => {
+        n.value = scheme[i % scheme.length];
+      });
+    };
   }
 
   reset() {
