4012c1bc602c801dc4635712e5d64a3dcc247c33
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-12-01 18:01:59 +0100

    disable droping more than 2 on imposition

--- src/ui/header.ts
+++ src/ui/header.ts
@@ -547,7 +547,7 @@ export function mergeDropAble(node: HTMLElement, column: Column, ctx: IRankingHe
       if (hasDnDType(e, ...numberish)) {
         node.dataset.draginfo = 'Wrap';
       }
-    });
+    }, false, () => column.children.length < 2);
   }
   if (column instanceof ImpositionBoxPlotColumn) {
     return dropAble(node, categorical.concat(boxplot), pushChild, (e) => {
@@ -558,7 +558,7 @@ export function mergeDropAble(node: HTMLElement, column: Column, ctx: IRankingHe
       if (hasDnDType(e, ...boxplot)) {
         node.dataset.draginfo = 'Wrap';
       }
-    });
+    }, false, () => column.children.length < 2);
   }
   if (column instanceof ImpositionCompositesColumn) {
     return dropAble(node, categorical.concat(numbers), pushChild, (e) => {
@@ -569,7 +569,7 @@ export function mergeDropAble(node: HTMLElement, column: Column, ctx: IRankingHe
       if (hasDnDType(e, ...numbers)) {
         node.dataset.draginfo = 'Wrap';
       }
-    });
+    }, false, () => column.children.length < 2);
   }
   if (isMultiLevelColumn(column)) {
     // stack column or nested
