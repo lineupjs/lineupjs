169bd48b482ab44c6c0e7d74a4aae2309754b2e5
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-06-04 12:24:01 +1000

    fix import

--- src/model/annotations.ts
+++ src/model/annotations.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
-import Column, {IColumnConstructor} from './Column';
-import {IColumnDesc} from './interfaces';
+import Column from './Column';
+import {IColumnDesc, IColumnConstructor} from './interfaces';
 
 const supportType = Symbol.for('SupportType');
 const category = Symbol.for('Category');
