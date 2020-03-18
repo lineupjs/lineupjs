39fa80c08ef08849981d2db71b5ef9c66cc53626
Samuel Gratzl <sam@sgratzl.com>
2020-03-18 11:00:39 -0400

    fix popup dialogs

--- src/ui/dialogs/APopup.ts
+++ src/ui/dialogs/APopup.ts
@@ -3,7 +3,7 @@ import ADialog, {IDialogContext, IDialogOptions} from "./ADialog";
 abstract class APopup extends ADialog {
   constructor(dialog: Readonly<IDialogContext>, options: Partial<IDialogOptions> = {}) {
     super(dialog, Object.assign({
-      popup: false
+      popup: true
     }, options));
   }
 
