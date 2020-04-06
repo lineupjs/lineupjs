663cbaa002b6969e5822cf23081b68c1ab959ff5
Samuel Gratzl <sam@sgratzl.com>
2020-04-06 10:48:40 +0200

    add simple e2e test

---
+++ cypress/integration/utils/ui.ts
@@ -0,0 +1,13 @@
+export function aggregateAll() {
+  cy.get('.lu-summary .lu-agg-expand').first().click();
+}
+
+export function closeDialog(action: 'cancel' | 'confirm' = 'confirm') {
+  cy.get(`.lu-dialog .lu-dialog-button[type=${action === 'cancel' ? 'button' : 'submit'}]`).click();
+  cy.get('.lu-backdrop').click();
+}
+
+export function openMoreDialog(column: string) {
+  // open more menu
+  cy.get(`.le-th${column} .lu-action-more`).first().click();
+}
