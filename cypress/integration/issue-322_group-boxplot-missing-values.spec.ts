6dc72986079399d44e7035776fb1ffee1e40d359
Holger Stitz <holger.stitz@datavisyn.io>
2020-05-13 12:54:14 +0200

    Add cypress to check for missing value class
    #322

---
+++ cypress/integration/issue-322_group-boxplot-missing-values.spec.ts
@@ -0,0 +1,38 @@
+import {withLineUp, waitReady, LineUpJSType, LineUp} from './utils/lineup';
+import {EAggregationState} from '../../src/model';
+// import {generateData} from './utils/data';
+
+describe('issue 322', () => {
+  let lineup: LineUp;
+  let lineUpJS: LineUpJSType;
+  before(withLineUp((l, document) => {
+    lineUpJS = l;
+
+    const arr = [
+      {string: "Row 0", number: NaN, cat: "c1"} // missing value for number column
+    ];
+
+    const builder = lineUpJS.builder(arr).deriveColumns().deriveColors();
+
+    const ranking = lineUpJS.buildRanking()
+      .aggregate()
+      .allColumns() // add all columns
+      .groupBy('cat')
+      .groupSortBy('cat', 'desc');
+
+    builder.ranking(ranking);
+
+    lineup = builder.build(document.body);
+
+    waitReady(lineup);
+  }));
+
+  it('Check for .lu-missing in boxplot renderer', () => {
+
+    lineup.data.aggregateAllOf(lineup.data.getFirstRanking(), EAggregationState.COLLAPSE);
+
+    waitReady(lineup);
+
+    cy.get('.le-tr[data-index="0"] > .lu-renderer-boxplot[data-group="g"]').should('have.class', 'lu-missing');
+  });
+});
