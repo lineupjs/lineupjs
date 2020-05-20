fa351c015cf343a2c99d8f60e08702013737cb13
Samuel Gratzl <sam@sgratzl.com>
2020-05-20 13:24:55 +0200

    join by space only

--- src/renderer/MultiLevelCellRenderer.ts
+++ src/renderer/MultiLevelCellRenderer.ts
@@ -160,7 +160,7 @@ export default class MultiLevelCellRenderer extends AAggregatedGroupRenderer<IMu
         }
         n.classList.add(cssClass('stack-label'));
 
-        const combinedLabel = overlapData.map((col) => col.label).join(', ');
+        const combinedLabel = overlapData.map((col) => col.label).join(' ');
         if (!combinedLabelNode) {
           combinedLabelNode = context.asElement(`<div class="${cssClass('bar-label')}"><span ${this.renderValue ? '' : `class="${cssClass('hover-only')}"`}></span></div>`);
         }
