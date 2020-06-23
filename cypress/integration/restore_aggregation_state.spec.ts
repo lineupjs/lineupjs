6985f0ef32ad081cb7873965f7548594c885ada3
Holger Stitz <holger.stitz@datavisyn.io>
2020-06-23 11:53:28 +0200

    Minor refactor + add logs and comments

--- cypress/integration/restore_aggregation_state.spec.ts
+++ cypress/integration/restore_aggregation_state.spec.ts
@@ -16,52 +16,63 @@ describe('restore_aggregation_state', () => {
 
   it('build and restore', () => {
     const data = lineup.data;
-    const log: {
+    const logs: {
       ranking: number,
       group: string | string[],
       topN: any;
     }[] = [];
+
+    // persist aggregation state in logs
     data.on('aggregate', (r, g, value, state) => {
-      console.log(r, g, value, state);
-      log.push({
+      console.log('on aggregate', r, g, value, state);
+      logs.push({
         ranking: data.getRankings().indexOf(r),
         group: Array.isArray(g) ? g.map((g) => g.name) : g.name,
         topN: state,
       });
-    })
+    });
+
     groupByString();
+
     cy.get('.lu-renderer-string.lu-group').first().should('contain', 'Row 0, Row 3');
     cy.get('.lu-renderer-string.lu-group').last().should('contain', 'Row 2, Row 20');
 
+    // store current aggregation state (to test correct restore)
     let old: any[];
     cy.get('.lu').wait(200).then(() => {
       const r = data.getFirstRanking();
       old = r.getGroups().map((g) => data.getAggregationState(r, g));
 
-      lineup.data.clearRankings();
-      lineup.data.deriveDefault();
+      data.clearRankings();
+      data.deriveDefault();
     });
+
     groupByString(false); // don't aggregate
   
+    // restore aggregation state from logs
     cy.get('.lu').wait(200).then(() => {
-      log.forEach((l) => {
-        const r = lineup.data.getRankings()[l.ranking];
-        if (Array.isArray(l.group)) {
-          const groups = r.getGroups().filter((g) => l.group.includes(g.name));
-          console.log(groups);
-          for (const g of groups) {
-            lineup.data.setTopNAggregated(r, g, l.topN);
+      console.log('logs', logs);
+      logs.forEach((log) => {
+        const ranking = data.getRankings()[log.ranking];
+        if (Array.isArray(log.group)) {
+          const groups = ranking.getGroups().filter((g) => log.group.includes(g.name));
+          console.log('groups', groups);
+          for (const group of groups) {
+            data.setTopNAggregated(ranking, group, log.topN);
           }
         } else {
-          const group = r.getGroups().find((g) => g.name == l.group);
+          const group = ranking.getGroups().find((g) => g.name == log.group);
           if (group) {
-            lineup.data.setTopNAggregated(r, group, l.topN);
+            data.setTopNAggregated(ranking, group, log.topN);
           }
         }
       });
+
+      // test aggregation state after restore
       const r = data.getFirstRanking();
-      expect(r.getGroups().map((g) => data.getAggregationState(r, g))).to.members(old);      
-    })
+      expect(r.getGroups().map((g) => data.getAggregationState(r, g))).to.members(old);
+    });
+
     cy.get('.lu-renderer-string.lu-group').first().should('contain', 'Row 0, Row 3');
     cy.get('.lu-renderer-string.lu-group').last().should('contain', 'Row 2, Row 20');
   });
