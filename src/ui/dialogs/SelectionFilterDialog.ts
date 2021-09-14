b537c51cdc3182160fcc3009def576959b2f9422
Samuel Gratzl <sam@sgratzl.com>
2021-09-14 10:07:05 -0400

    style: tune selectio style filter

--- src/ui/dialogs/SelectionFilterDialog.ts
+++ src/ui/dialogs/SelectionFilterDialog.ts
@@ -45,6 +45,13 @@ export default class SelectionFilterDialog extends ADialog {
     return true;
   }
 
+  private guessRemoveFilter() {
+    const total = this.ctx.provider.getTotalNumberOfRows();
+    const ranking = this.column.findMyRanker();
+    const hasOtherFilter = ranking.flatColumns.some((d) => d.isFiltered() && d !== this.column);
+    return hasOtherFilter ? '?' : total.toLocaleString();
+  }
+
   protected build(node: HTMLElement) {
     const filtered = this.column.getFilter() ?? [];
     const selected = this.ctx.provider.getSelection().length;
@@ -59,45 +66,66 @@ export default class SelectionFilterDialog extends ADialog {
       node.insertAdjacentHTML(
         'beforeend',
         `<div class="${cssClass('dialog-table')}">
-        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')}">
+        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')} ${cssClass(
+          'dialog-table-entry-wide'
+        )}">
           <input type="radio" checked="checked" name="mode" value="none">
           <span>
             <div class="${cssClass('dialog-filter-table-entry-label')}">No filter</div>
-            <div class="${cssClass('dialog-filter-table-entry-stats')}">${visible}/${total}</div>
+            <div class="${cssClass(
+              'dialog-filter-table-entry-stats'
+            )}">${visible.toLocaleString()}/${total.toLocaleString()}</div>
           </span>
         </label>
-        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')}">
+        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')} ${cssClass(
+          'dialog-table-entry-wide'
+        )}">
           <input type="radio" name="mode" value="update">
           <span>
             <div class="${cssClass('dialog-filter-table-entry-label')}">Set filter to current selection</div>
-            <div class="${cssClass('dialog-filter-table-entry-stats')}">${selected}/${total}</div>
+            <div class="${cssClass(
+              'dialog-filter-table-entry-stats'
+            )}">${selected.toLocaleString()}/${total.toLocaleString()}</div>
           </span>
         </label>
       </div>`
       );
     } else {
+      const guessedRemoved = this.guessRemoveFilter();
       node.insertAdjacentHTML(
         'beforeend',
         `<div class="${cssClass('dialog-table')}" >
-        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')}">
+        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')} ${cssClass(
+          'dialog-table-entry-wide'
+        )}">
           <input type="radio" ${filtered.length === 0 ? 'checked="checked"' : ''} name="mode" value="none">
           <span>
             <div class="${cssClass('dialog-filter-table-entry-label')}">Remove filter</div>
-            <div class="${cssClass('dialog-filter-table-entry-stats')}">?/${total}</div>
+            <div class="${cssClass(
+              'dialog-filter-table-entry-stats'
+            )}">${guessedRemoved}/${total.toLocaleString()}</div>
           </span>
         </label>
-        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')}">
+        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')} ${cssClass(
+          'dialog-table-entry-wide'
+        )}">
           <input type="radio" ${filtered.length > 0 ? 'checked="checked"' : ''} name="mode" value="keep">
           <span>
             <div class="${cssClass('dialog-filter-table-entry-label')}">Keep current filter</div>
-            <div class="${cssClass('dialog-filter-table-entry-stats')}">${filtered.length}/${total}</div>
+            <div class="${cssClass(
+              'dialog-filter-table-entry-stats'
+            )}">${filtered.length.toLocaleString()}/${total.toLocaleString()}</div>
           </span>
         </label>
-        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')}">
+        <label class="${cssClass('checkbox')} ${cssClass('dialog-filter-table-entry')} ${cssClass(
+          'dialog-table-entry-wide'
+        )}">
           <input type="radio" name="mode" value="update" ${selected === 0 ? 'disabled="disabled"' : ''}>
           <span>
             <div class="${cssClass('dialog-filter-table-entry-label')}">Set filter to current selection</div>
-            <div class="${cssClass('dialog-filter-table-entry-stats')}">${selected}/${total}</div>
+            <div class="${cssClass(
+              'dialog-filter-table-entry-stats'
+            )}">${selected.toLocaleString()}/${total.toLocaleString()}</div>
           </span>
         </label>
       </div>`
