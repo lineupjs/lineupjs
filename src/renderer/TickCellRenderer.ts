dde871baafa5472d6c6af5e96da09e9c5566c78f
Samuel Gratzl <sam@sgratzl.com>
2021-12-23 09:37:56 -0500

    feat: label for tick vis

--- src/renderer/TickCellRenderer.ts
+++ src/renderer/TickCellRenderer.ts
@@ -1,6 +1,6 @@
 import { round } from '../internal';
 import { Column, IDataRow, INumberColumn, isNumberColumn } from '../model';
-import { CANVAS_HEIGHT, TICK } from '../styles';
+import { CANVAS_HEIGHT, cssClass, TICK } from '../styles';
 import { colorOf } from './impose';
 import {
   ERenderMode,
@@ -17,6 +17,12 @@ import { adaptColor, noRenderer, SMALL_MARK_SATURATION_FACTOR } from './utils';
 export default class DotCellRenderer implements ICellRendererFactory {
   readonly title: string = 'Tick';
   readonly groupTitle: string = 'Ticks';
+  /**
+   * flag to always render the value
+   * @type {boolean}
+   */
+
+  constructor(private readonly renderValue: boolean = false) {}
 
   canRender(col: Column, mode: ERenderMode): boolean {
     return isNumberColumn(col) && mode === ERenderMode.CELL;
@@ -25,17 +31,20 @@ export default class DotCellRenderer implements ICellRendererFactory {
   create(col: INumberColumn, context: IRenderContext, imposer?: IImposer): ICellRenderer {
     const width = col.getWidth();
     return {
-      template: `<div><div></div></div>`,
+      template: `<div><div></div><span ${
+        this.renderValue ? '' : `class="${cssClass('text-shadow')} ${cssClass('hover-only')}"`
+      }></span></div>`,
       update: (n: HTMLDivElement, d: IDataRow) => {
         renderMissingDOM(n, col, d);
         const color = adaptColor(colorOf(col, d, imposer), SMALL_MARK_SATURATION_FACTOR);
-        const l = col.getLabel(d);
+        const l = context.sanitize(col.getLabel(d));
         const v = col.getNumber(d);
         n.title = l;
         const tick = n.firstElementChild as HTMLElement;
-        tick.title = context.sanitize(l);
         tick.style.background = context.sanitize(color);
         tick.style.left = `${round(v * 100, 2)}%`;
+        const label = n.lastElementChild as HTMLElement;
+        label.textContent = l;
       },
       render: (ctx: CanvasRenderingContext2D, d: IDataRow) => {
         if (renderMissingCanvas(ctx, col, d, width)) {
