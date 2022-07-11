a3e7cf20f4e006d4bba9d8ee31d903001b34f1df
Holger Stitz <holger.stitz@datavisyn.io>
2022-07-11 14:53:07 +0200

    feat: add option for custom instance id

---
+++ cypress/integration/custom_instance_id.spec.ts
@@ -0,0 +1,19 @@
+import { withLineUp, waitReady, LineUpJSType, Taggle } from './utils/lineup';
+
+describe('builder', () => {
+  let lineup: Taggle;
+  let lineUpJS: LineUpJSType;
+  before(
+    withLineUp((l, document) => {
+      lineUpJS = l;
+      const b = lineUpJS.builder([]);
+      b.options.instanceId = 'custom-instance-id';
+      b.build(document.body);
+      waitReady(lineup);
+    })
+  );
+
+  it('default', () => {
+    cy.get('main.le').should('have.id', 'lu-custom-instance-id');
+  });
+});
