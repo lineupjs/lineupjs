915e23bab785674c3563052ad0ecfd1c96411f02
oltionchampari <oltion.champari@datavisyn.io>
2026-03-09 10:55:09 +0100

    fix: remove the group and summary renderers

--- src/renderer/CategoricalsHistogramCellRenderer.ts
+++ src/renderer/CategoricalsHistogramCellRenderer.ts
@@ -1,32 +1,14 @@
 import { categoricalStatsBuilder } from '../internal';
-import {
-  isCategoricalsColumn,
-  type ICategoricalsColumn,
-  type Column,
-  type IDataRow,
-  type IOrderedGroup,
-} from '../model';
-import { cssClass } from '../styles';
-import {
-  ERenderMode,
-  type ICellRenderer,
-  type ICellRendererFactory,
-  type IGroupCellRenderer,
-  type IRenderContext,
-  type ISummaryRenderer,
-} from './interfaces';
+import { isCategoricalsColumn, type ICategoricalsColumn, type Column, type IDataRow } from '../model';
+import { ERenderMode, type ICellRenderer, type ICellRendererFactory, type IRenderContext } from './interfaces';
 import { renderMissingDOM } from './missing';
-import { wideEnough } from './utils';
 import { categoricalHistogram } from './categoricalHistogram';
 
 export default class CategoricalsHistogramCellRenderer implements ICellRendererFactory {
   readonly title: string = 'Histogram';
 
   canRender(col: Column, mode: ERenderMode): boolean {
-    return (
-      isCategoricalsColumn(col) &&
-      (mode === ERenderMode.CELL || mode === ERenderMode.GROUP || mode === ERenderMode.SUMMARY)
-    );
+    return isCategoricalsColumn(col) && mode === ERenderMode.CELL;
   }
 
   create(col: ICategoricalsColumn, context: IRenderContext): ICellRenderer {
@@ -42,50 +24,6 @@ export default class CategoricalsHistogramCellRenderer implements ICellRendererF
         const b = categoricalStatsBuilder(col.categories);
         col.iterCategory(row).forEach((cat) => b.push(cat));
         update(n, b.build());
-      },
-    };
-  }
-
-  createGroup(col: ICategoricalsColumn, context: IRenderContext): IGroupCellRenderer {
-    const { template, update, matchBins } = categoricalHistogram(col, false, context.sanitize);
-
-    return {
-      template: `${template}</div>`,
-      update: (n: HTMLElement, group: IOrderedGroup) => {
-        matchBins(n);
-        return context.tasks.groupCategoricalStats(col, group).then((r) => {
-          if (typeof r === 'symbol') {
-            return;
-          }
-          const isMissing = !r || r.group == null || r.group.count === 0 || r.group.count === r.group.missing;
-          n.classList.toggle(cssClass('missing'), isMissing);
-          if (isMissing) {
-            return;
-          }
-          update(n, r.group);
-        });
-      },
-    };
-  }
-
-  createSummary(col: ICategoricalsColumn, context: IRenderContext, interactive: boolean): ISummaryRenderer {
-    const { template, update, matchBins } = categoricalHistogram(col, interactive || wideEnough(col), context.sanitize);
-
-    return {
-      template: `${template}</div>`,
-      update: (n: HTMLElement) => {
-        matchBins(n);
-        return context.tasks.summaryCategoricalStats(col).then((r) => {
-          if (typeof r === 'symbol') {
-            return;
-          }
-          const isMissing = !r || r.summary == null || r.summary.count === 0 || r.summary.count === r.summary.missing;
-          n.classList.toggle(cssClass('missing'), isMissing);
-          if (isMissing) {
-            return;
-          }
-          update(n, r.summary);
-        });
       },
     };
   }
